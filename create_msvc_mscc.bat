3307e54ac9322fbce35cf2fe338e4e0c75f7b1e4
Piotr Majchrzak <piotr.majchrzak@intel.com>
2016-02-22 15:38:59 +0100

    unrem one line in bat file

--- create_msvc_mscc.bat
+++ create_msvc_mscc.bat
@@ -5,6 +5,6 @@
 echo Creating Microsoft Visual Studio 14 2015 Win64 files...
 
 cmake -G"Visual Studio 14 2015 Win64" -B".\MSVC\Debug" --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING="Debug" -DCMAKE_CONFIGURATION_TYPES:STRING="Debug" -DCMAKE_SUPPRESS_REGENERATION:BOOL=ON -H"."
-REM cmake -G"Visual Studio 14 2015 Win64" -B".\MSVC\Release" --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_CONFIGURATION_TYPES:STRING="Release" -DCMAKE_SUPPRESS_REGENERATION:BOOL=ON -H"."
+cmake -G"Visual Studio 14 2015 Win64" -B".\MSVC\Release" --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_CONFIGURATION_TYPES:STRING="Release" -DCMAKE_SUPPRESS_REGENERATION:BOOL=ON -H"."
 echo Done.
 pause
