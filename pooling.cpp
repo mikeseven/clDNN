62a6745f18d49f0c52cc56ba153e7579e4139037
Jakub Grzybek <jakub.grzybek@intel.com>
2016-02-22 09:00:36 +0100

    todo: pooling

--- pooling.cpp
+++ pooling.cpp
@@ -2,18 +2,26 @@
 
 namespace neural {
 
-pooling::arguments::arguments(neural::engine, pooling::mode, memory::format o_frmt, std::vector<uint32_t> out_off, std::vector<uint32_t> out_siz, primitive in, std::vector<int32_t> in_off, std::vector<uint32_t> stride, std::vector<uint32_t> size, neural::padding);
-
+pooling::arguments::arguments( neural::engine eng,
+                               pooling::mode mode,
+                               memory::format o_frmt,
+                               std::vector<uint32_t> out_off,
+                               std::vector<uint32_t> out_siz,
+                               primitive in,
+                               std::vector<int32_t> in_off,
+                               std::vector<uint32_t> arg_stride,
+                               std::vector<uint32_t> arg_size,
+                               neural::padding apadd)
     : engine(eng)
     , mode(mode)
-    , output({out})
-    , output_offset({out.as<const memory&>().argument.size.size()})
-    , output_size(out.as<const memory&>().argument.size.begin(), out.as<const memory&>().argument.size.end())
+    , output( {memory::create({eng, o_frmt, out_siz})} )
+    , output_offset(out_off)
+    , output_size(out_siz)
     , input({in})
-    , input_offset({in.as<const memory&>().argument.size.size()})
-    , stride(stride)
-    , size({0}) //todo ?
-    , padding(padd)
+    , input_offset(in_off)
+    , stride(arg_stride)
+    , size(arg_size)
+    , padding(apadd)
 {}
 
 }
