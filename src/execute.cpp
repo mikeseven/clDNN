56cd212c9de167a4ab323bcb182d8bfd020b62b1
Jakub Grzybek <jakub.grzybek@intel.com>
2016-03-07 16:15:33 +0100

    added convolution zero padding

--- src/execute.cpp
+++ src/execute.cpp
@@ -1,15 +1,21 @@
 #include "api/neural_base.h"
 #include "thread_pool.h"
 
+#include <iostream>
+
 namespace neural {
 
 nn_thread_worker_pool thread_pool;
 
 void execute(std::vector<primitive> list) {
-    for(auto &item : list) {
-        thread_pool.push_job(item.work());
+    try {
+        for(auto &item : list)
+            thread_pool.push_job(item.work());
+    } catch (std::exception &e) {
+        std::cerr << e.what();
+    } catch (...) {
+        std::cerr << "Unknown exception has been thrown.";
     }
-
 }
 
 
