8c073c5c4311ee43b55da8284b84744928607874
Artur Lesniak <artur.lesniak@intel.com>
2016-03-11 09:56:16 +0100

    in progress

--- src/reorder.cpp
+++ src/reorder.cpp
@@ -8,14 +8,64 @@
 namespace neural {
 
 	namespace {
-        template <typename T>
-            class Axis<T> {
-                memory::format::type fmt;
+
+        class Dims{
+            std::vector<char> order;
+            memory::format::type format;
+
+            Dims(memory::format::type _fmt, std::vector<char> _order):format(_fmt), order(_order){};
+
+            static std::vector<char> getOrder(memory::format::type fmt){
+                switch(fmt) {
+                case memory::format::  xb_f32: return {'x','b'};
+                case memory::format::yxfb_f32: return {'y','x','f','b'};
+                case memory::format::fyxb_f32: return {'f','y','x','b'};
+                case memory::format::xyfb_f32: return {'x','y','f','b'};
+                case memory::format::fxyb_f32: return {'f','x','y','b'};
+                case memory::format::byxf_f32: return {'b','y','x','f'};
+                case memory::format::bfyx_f32: return {'b','f','y','x'};
+                case memory::format::bxyf_f32: return {'b','x','y','f'};
+		        case memory::format::bfxy_f32: return {'b','f','x','y'};
+                case memory::format::yxfb_f64: return {'y','x','f','b'};
+                case memory::format::fyxb_f64: return {'f','y','x','b'};
+                case memory::format::xyfb_f64: return {'x','y','f','b'};
+                case memory::format::fxyb_f64: return {'f','x','y','b'};
+                case memory::format::byxf_f64: return {'b','y','x','f'};
+                case memory::format::bfyx_f64: return {'b','f','y','x'};
+                case memory::format::bxyf_f64: return {'b','x','y','f'};
+                case memory::format::bfxy_f64: return {'b','f','x','y'};
+                default: throw std::runtime_error("unknown memory::format");
+                };
+            };
+        public:
+            static Dims create(memory::format::type fmt){
+                return {fmt,getOrder(fmt)};
+            };
+
+            template <typename T>
+            std::vector<T>&  translatePos(Dims _other) {
+                return translatePos(_other.order,_other.format);
+            };
+            template <typename T>
+            std::vector<T>&  translatePos(std::vector<T>& _srcPosition, const memory::format::type& _srcFormat) {
+	            std::vector<T> tmp_pos(0);
+	            for(int i =0; i< _srcPosition.size();++i){
+		            auto o = this->order[i];
+		            for(int j =0; j <  _srcPosition.size();++j)
+		            {
+			            auto p = getOrder(_srcFormat)[j];
+			            if(o == p)
+				            tmp_pos.push_back(_srcPosition[j]);
+		            }
+	            }
+                _srcPosition.clear();
+                for(auto p:tmp_pos) {
+                    _srcPosition.push_back(p);
+                }
+                return _srcPosition;
+            };
         };
 
-        size_t reorder_reference::Axis operator()(memory::format::type fmt){
-
-        };
 		struct reorder_reference : is_an_implementation {
 			const reorder &outer;
 			reorder_reference(reorder &arg)
@@ -45,16 +95,21 @@ namespace neural {
 
 				namespace nd = ndimensional;
 				nd::value<uint32_t> range (output_size);
+                Dims dimRange = Dims::create(output_format);
 
-                nd::calculate_idx<uint32_t> *calc_in_idx = nullptr;
-                if(input_format == memory::format::yxfb_f32 && output_format == memory::format::bfxy_f32)
-                    calc_in_idx = new nd::calculate_idx<uint32_t>({input_size[3],input_size[2],input_size[1],input_size[0]});
-                else
-                    calc_in_idx = new nd::calculate_idx<uint32_t>(output_size);
-
+                //nd::calculate_idx<uint32_t> *calc_in_idx = nullptr;
+                //if(input_format == memory::format::yxfb_f32 && output_format == memory::format::bfxy_f32)
+                //    calc_in_idx = new nd::calculate_idx<uint32_t>({input_size[3],input_size[2],input_size[1],input_size[0]});
+                //else
+                //    calc_in_idx = new nd::calculate_idx<uint32_t>(output_size);
+                nd::calculate_idx<uint32_t> calc_in_idx (input_size);
+                Dims dimInput = Dims::create(input_format);
 				nd::calculate_idx<uint32_t> calc_out_idx (output_size);
+                Dims dimOutput = Dims::create(output_format);
 				for(auto pos : range) {
-					auto in_idx  = (*calc_in_idx)(pos);
+					//auto in_idx  = calc_in_idx(pos);
+                    auto in_idx  = calc_in_idx(dimInput.translatePos(dimRange));
+                    //auto in_idx  = calc_in_idx((Dims::create(input_format)).translatePos(input_size,input_format));
 					auto out_idx = calc_out_idx(pos);
 
 					output[out_idx] = input[in_idx];
