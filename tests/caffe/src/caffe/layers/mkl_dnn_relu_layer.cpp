cfc83067e54834cd617bb3b7779ac3ce1e619d82
Jacek Reniecki <jacek.reniecki@intel.com>
2016-04-26 12:20:14 +0200

    Changed caffe/relu integr for new sizes format

--- tests/caffe/src/caffe/layers/mkl_dnn_relu_layer.cpp
+++ tests/caffe/src/caffe/layers/mkl_dnn_relu_layer.cpp
@@ -27,15 +27,12 @@ void MKL_DNNReLULayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
 
 //  CHECK_EQ(top[0]->shape(), bottom[0]->shape());
 
-  vector<unsigned> sizes;
-  for (auto d : bottom[0]->shape())
-      sizes.push_back(d);
-
+  auto s = bottom[0]->shape();
   // TODO: change format?
-  bottom_data_  = memory::create({engine::cpu, memory::format::yxfb_f32, sizes});
-  top_data_     = memory::create({engine::cpu, memory::format::yxfb_f32, sizes});
-  bottom_diff_  = memory::create({engine::cpu, memory::format::yxfb_f32, sizes});
-  top_diff_     = memory::create({engine::cpu, memory::format::yxfb_f32, sizes});
+  bottom_data_  = memory::create({engine::cpu, memory::format::yxfb_f32, {s[3], {s[1],s[0]}, s[2]}});
+  top_data_     = memory::create({engine::cpu, memory::format::yxfb_f32, {s[3], {s[1],s[0]}, s[2]}});
+  bottom_diff_  = memory::create({engine::cpu, memory::format::yxfb_f32, {s[3], {s[1],s[0]}, s[2]}});
+  top_diff_     = memory::create({engine::cpu, memory::format::yxfb_f32, {s[3], {s[1],s[0]}, s[2]}});
 
   reluFwd_ = relu::create({engine::reference, top_data_, bottom_data_, negative_slope});
   reluBwd_ = relu_backward::create({engine::reference, {bottom_diff_}, {top_diff_, bottom_data_}, negative_slope});
