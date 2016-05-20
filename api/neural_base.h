ca218004e2cde089b555d65316e4e603012d5dfd
murbansk <maciej.urbanski@intel.com>
2016-05-20 12:08:08 +0200

    adding volatile so release build will not optimize-away sync

--- api/neural_base.h
+++ api/neural_base.h
@@ -433,8 +433,8 @@ public:
 
 // asynchronous result
 class async_result {
-    std::shared_ptr<uint32_t> _tasks_left;
-    async_result(std::shared_ptr<uint32_t> arg) : _tasks_left(arg) {}
+    std::shared_ptr<volatile uint32_t> _tasks_left;
+    async_result(std::shared_ptr<volatile uint32_t> arg) : _tasks_left(arg) {}
 
     // execution of sequence of primitives
     friend DLL_SYM async_result execute(std::vector<primitive>, worker);
