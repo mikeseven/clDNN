89650bd7c18f5a03154b73d670dc790ae47206e5
Agata Radys <agata.radys@intel.com>
2016-05-31 15:26:53 +0200

    thread pool initial

---
+++ tests/test_cases/thread_pool_test.cpp
@@ -0,0 +1,56 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "api/neural.h"
+#include "tests/gtest/gtest.h"
+#include "test_utils/test_utils.h"
+
+using namespace neural;
+using namespace tests;
+
+void do_some (const void* a)
+{
+    long  sum = 0;
+    int count = rand() % 10;
+    for (long i = 0; i < count; i++)
+    {
+        sum += i*i;
+    }
+    void* a_temp = (void*)(a);
+    int* b = static_cast<int*>(a_temp);
+    *b = sum;
+    //*b = *b+1;
+    //cout << "number=" << *static_cast<const int*>(a) << endl;
+    //cout << *static_cast<const int*>(a);
+}
+
+TEST(thread_pool, check_result) {
+
+    task_group tasks;
+    const int task_count = 100000;
+    int num[task_count];
+    for (int i = 0; i < task_count; i++)
+    {
+        num[i] = i;
+        tasks.tsk.push_back({ &do_some, &num[i] });
+    }
+
+    nn_thread_worker_pool wp();
+    wp.push_job(tasks);
+
+}
