d150bad8f4d04cd5a970a4e16a2f5f726f4ee718
jreniecki <jacek.reniecki@intel.com>
2016-06-02 11:30:31 +0200

    Caffe integration: support for optimized pooling (engine::cpu)

--- src/reorder.cpp
+++ src/reorder.cpp
@@ -136,9 +136,12 @@ namespace neural {
         static std::map<implementation_key, std::function<is_an_implementation *(reorder &)>> implementation_map = {
             { std::make_tuple(engine::reference, memory::format::oiyx_f32, memory::format::os_yxi_sv16_f32), reorder_reference::create },
             { std::make_tuple(engine::reference, memory::format::os_yxi_sv16_f32, memory::format::oiyx_f32), reorder_reference::create },
-            
+
             { std::make_tuple(engine::reference, memory::format::bs_yxf_bv24_f32, memory::format::yxfb_f32), reorder_reference::create },
             { std::make_tuple(engine::reference, memory::format::yxfb_f32, memory::format::bs_yxf_bv24_f32), reorder_reference::create },
+
+            { std::make_tuple(engine::reference, memory::format::bs_yxf_bv24_f32, memory::format::bfyx_f32), reorder_reference::create },
+            { std::make_tuple(engine::reference, memory::format::bfyx_f32, memory::format::bs_yxf_bv24_f32), reorder_reference::create },
 
             { std::make_tuple(engine::reference, memory::format::x_f32, memory::format::x_f32), reorder_reference::create },
             { std::make_tuple(engine::reference, memory::format::yxfb_f32, memory::format::yxfb_f32), reorder_reference::create },
