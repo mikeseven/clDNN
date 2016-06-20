bc377c9787992b0dd6415913529c05ff1c496a5e
kbadziak <krzysztof.badziak@intel.com>
2016-06-20 13:05:36 +0200

    some speedup

--- src/reorder_cpu.cpp
+++ src/reorder_cpu.cpp
@@ -29,8 +29,239 @@ struct op_data_t {
     uint32_t size_y;
     uint32_t size_x;
     uint32_t size_f;
+
+    uint32_t row;
 };
 }
+
+struct ULTRAFAST_reorder_cpu_byxf_f32_to_byxf_b24_f32 : is_an_implementation {
+    const reorder &outer;
+
+    std::vector<task> tasks;
+    std::vector<op_data_t> op_array;
+
+    ULTRAFAST_reorder_cpu_byxf_f32_to_byxf_b24_f32(reorder &arg)
+        : is_an_implementation(neural::type_id<ULTRAFAST_reorder_cpu_byxf_f32_to_byxf_b24_f32>())
+        , outer(arg)
+    {
+        const uint32_t batch = outer.input_memory(0).argument.size.batch;
+
+        tasks.resize(batch, {implementation, nullptr});
+        op_array.resize(batch, {0
+                        , reinterpret_cast<float**>(&outer.output_memory(0).pointer)
+                        , reinterpret_cast<float**>(&outer.input_memory(0).pointer)
+                        , outer.input_memory(0).argument.size.batch[0]
+                        , outer.input_memory(0).argument.size.spatial[1]
+                        , outer.input_memory(0).argument.size.spatial[0]
+                        , outer.input_memory(0).argument.size.feature[0]
+                        , 0
+        });
+
+        for(uint32_t b = 0; b < batch; ++b){
+            op_array[b].batch = b;
+            tasks[b].data     = &op_array[b];
+        }
+    }
+    ~ULTRAFAST_reorder_cpu_byxf_f32_to_byxf_b24_f32() {}
+    task_group work() { return {tasks, schedule::unordered}; }
+    static is_an_implementation *create(reorder &arg) { return new ULTRAFAST_reorder_cpu_byxf_f32_to_byxf_b24_f32(arg); }
+
+    static void implementation(const void *ptr) {
+        auto data       = static_cast<const op_data_t* >(ptr);
+        auto input_ptr  = *data->input;
+        auto output_ptr = *data->output;
+
+        const uint64_t size_y = data->size_y;
+        const uint64_t size_x = data->size_x;
+        const uint64_t size_f = data->size_f;
+        const uint64_t b = data->batch;
+
+        //auto outoffs = b%24 + 24 * (f + size_f * (x + size_x * (y + (b/24) * size_y)));
+        //auto inoffs = f + size_f * (x + size_x * (y + size_y * b));
+
+        if(size_f % 8 == 0)
+        {
+            // Precomputed constants.
+            const uint64_t fm_package_size = 8;
+            const uint64_t in_f_stride = fm_package_size;
+            const uint64_t out_f_stride = fm_package_size*24;
+            const uint64_t in_xf_stride = size_f;
+            const uint64_t out_xf_stride = size_f * 24;
+            const uint64_t in_yxf_stride = size_x * size_f;
+            const uint64_t out_yxf_stride = size_x * size_f * 24;
+
+            auto in_b_offset = b * size_y * size_x * size_f;
+            auto out_b_offset = b%24 + (b/24) * size_y * size_x * size_f * 24;
+
+            for(uint64_t y=0; y<size_y; ++y)
+            {
+                auto in_by_offset = in_b_offset;
+                auto out_by_offset = out_b_offset;
+
+                for(uint64_t x=0; x<size_x; ++x)
+                {
+                    auto in_byx_offset = in_by_offset;
+                    auto out_byx_offset = out_by_offset;
+
+                    for(uint64_t f = 0; f < size_f; f += fm_package_size)
+                    {
+                        output_ptr[out_byx_offset + 0 * 24] = input_ptr[in_byx_offset + 0];
+                        output_ptr[out_byx_offset + 1 * 24] = input_ptr[in_byx_offset + 1];
+                        output_ptr[out_byx_offset + 2 * 24] = input_ptr[in_byx_offset + 2];
+                        output_ptr[out_byx_offset + 3 * 24] = input_ptr[in_byx_offset + 3];
+                        output_ptr[out_byx_offset + 4 * 24] = input_ptr[in_byx_offset + 4];
+                        output_ptr[out_byx_offset + 5 * 24] = input_ptr[in_byx_offset + 5];
+                        output_ptr[out_byx_offset + 6 * 24] = input_ptr[in_byx_offset + 6];
+                        output_ptr[out_byx_offset + 7 * 24] = input_ptr[in_byx_offset + 7];
+
+                        in_byx_offset += in_f_stride;
+                        out_byx_offset += out_f_stride;
+                    }
+
+                    in_by_offset += in_xf_stride;
+                    out_by_offset += out_xf_stride;
+                }
+
+                in_b_offset += in_yxf_stride;
+                out_b_offset += out_yxf_stride;
+            }
+        }
+        else
+        {
+            //throw std::runtime_error("this code works, its just a debug print for evaluation purposes ;)");
+
+            // Precomputed constants.
+            const uint64_t fm_package_size = 1;
+            const uint64_t in_f_stride = fm_package_size;
+            const uint64_t out_f_stride = fm_package_size*24;
+            const uint64_t in_xf_stride = size_f;
+            const uint64_t out_xf_stride = size_f * 24;
+            const uint64_t in_yxf_stride = size_x * size_f;
+            const uint64_t out_yxf_stride = size_x * size_f * 24;
+
+            auto in_b_offset = b * size_y * size_x * size_f;
+            auto out_b_offset = b%24 + (b/24) * size_y * size_x * size_f * 24;
+
+            for(uint64_t y=0; y<size_y; ++y)
+            {
+                auto in_by_offset = in_b_offset;
+                auto out_by_offset = out_b_offset;
+
+                for(uint64_t x=0; x<size_x; ++x)
+                {
+                    auto in_byx_offset = in_by_offset;
+                    auto out_byx_offset = out_by_offset;
+
+                    for(uint64_t f = 0; f < size_f; f += fm_package_size)
+                    {
+                        output_ptr[out_byx_offset] = input_ptr[in_byx_offset];
+
+                        in_byx_offset += in_f_stride;
+                        out_byx_offset += out_f_stride;
+                    }
+
+                    in_by_offset += in_xf_stride;
+                    out_by_offset += out_xf_stride;
+                }
+
+                in_b_offset += in_yxf_stride;
+                out_b_offset += out_yxf_stride;
+            }
+        }
+    }
+};
+
+struct ULTRAFAST_reorder_cpu_byxf_b24_f32_to_fyxb_f32 : is_an_implementation {
+    const reorder &outer;
+
+    std::vector<task> tasks;
+    std::vector<op_data_t> op_array;
+
+    ULTRAFAST_reorder_cpu_byxf_b24_f32_to_fyxb_f32(reorder &arg)
+        : is_an_implementation(neural::type_id<ULTRAFAST_reorder_cpu_byxf_b24_f32_to_fyxb_f32>())
+        , outer(arg)
+    {
+        const uint32_t row = outer.input_memory(0).argument.size.spatial[1];
+
+        tasks.resize(row, {implementation, nullptr});
+        op_array.resize(row, {0
+                        , reinterpret_cast<float**>(&outer.output_memory(0).pointer)
+                        , reinterpret_cast<float**>(&outer.input_memory(0).pointer)
+                        , outer.input_memory(0).argument.size.batch[0]
+                        , outer.input_memory(0).argument.size.spatial[1]
+                        , outer.input_memory(0).argument.size.spatial[0]
+                        , outer.input_memory(0).argument.size.feature[0]
+                        , 0
+        });
+
+        for(uint32_t r = 0; r < row; ++r){
+            op_array[r].row = r;
+            tasks[r].data     = &op_array[r];
+        }
+    }
+    ~ULTRAFAST_reorder_cpu_byxf_b24_f32_to_fyxb_f32() {}
+    task_group work() { return {tasks, schedule::unordered}; }
+    static is_an_implementation *create(reorder &arg) { return new ULTRAFAST_reorder_cpu_byxf_b24_f32_to_fyxb_f32(arg); }
+
+    static void implementation(const void *ptr) {
+        auto data       = static_cast<const op_data_t* >(ptr);
+        auto input_ptr  = *data->input;
+        auto output_ptr = *data->output;
+
+        const uint64_t size_y = data->size_y;
+        const uint64_t size_x = data->size_x;
+        const uint64_t size_f = data->size_f;
+        const uint64_t size_b = data->size_b;
+        const uint64_t y = data->row;
+
+        //auto inoffs = b%24 + 24 * (f + size_f * (x + size_x * (y + (b/24) * size_y)));
+        //auto outoffs = b + size_b * (x + size_x * (y + size_y * f));
+
+        // Precomputed constants.
+        const uint64_t in_f_stride = 24;
+        const uint64_t out_f_stride = size_y * size_x * size_b;
+        const uint64_t in_xf_stride = size_f * 24;
+        const uint64_t out_xf_stride = size_b;
+        const uint64_t in_yxf_stride = size_y * size_x * size_f * 24;
+        const uint64_t out_yxf_stride = 24;
+
+        {
+            auto in_y_offset = y * size_x * size_f * 24;
+            auto out_y_offset = y * size_x * size_b;
+
+            for(uint64_t b=0; b<size_b; b+=24)
+            {
+                auto in_yb_offset = in_y_offset;
+                auto out_yb_offset = out_y_offset;
+
+                for(uint64_t x=0; x<size_x; ++x)
+                {
+                    auto in_xyb_offset = in_yb_offset;
+                    auto out_xyb_offset = out_yb_offset;
+
+                    for(uint64_t f=0; f<size_f; ++f)
+                    {
+                            __m256 acc0 = _mm256_loadu_ps(&input_ptr[in_xyb_offset +  0]);
+                            __m256 acc1 = _mm256_loadu_ps(&input_ptr[in_xyb_offset +  8]);
+                            __m256 acc2 = _mm256_loadu_ps(&input_ptr[in_xyb_offset + 16]);
+                            _mm256_storeu_ps(&output_ptr[out_xyb_offset +  0], acc0);
+                            _mm256_storeu_ps(&output_ptr[out_xyb_offset +  8], acc1);
+                            _mm256_storeu_ps(&output_ptr[out_xyb_offset + 16], acc2);
+
+                            in_xyb_offset += in_f_stride;
+                            out_xyb_offset += out_f_stride;
+                    }
+
+                    in_yb_offset += in_xf_stride;
+                    out_yb_offset += out_xf_stride;
+                }
+
+                in_y_offset += in_yxf_stride;
+                out_y_offset += out_yxf_stride;
+            }
+        }
+    }
+};
 
 struct reorder_cpu_byxf_f32_to_byxf_b24_f32 : is_an_implementation {
     const reorder &outer;
@@ -194,8 +425,8 @@ namespace {
         attach() {
             auto &map = reorder_fw_implementation_map::instance();
                                       // engine       output                        input                      create function
-            map.insert( {std::make_tuple(engine::cpu, memory::format::byxf_b24_f32, memory::format::byxf_f32), reorder_cpu_byxf_f32_to_byxf_b24_f32::create} );
-            map.insert( {std::make_tuple(engine::cpu, memory::format::byxf_f32,     memory::format::bfyx_f32), reorder_cpu_bfyx_f32_to_byxf_f32    ::create} );
+            map.insert( {std::make_tuple(engine::cpu, memory::format::byxf_b24_f32, memory::format::byxf_f32), ULTRAFAST_reorder_cpu_byxf_f32_to_byxf_b24_f32::create} );
+            map.insert( {std::make_tuple(engine::cpu, memory::format::fyxb_f32,     memory::format::byxf_b24_f32), ULTRAFAST_reorder_cpu_byxf_b24_f32_to_fyxb_f32::create} );
         }
         ~attach(){}
     };
