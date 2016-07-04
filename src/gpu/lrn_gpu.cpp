08d685b268cefc62a52f071bea4f1139da5b1512
Michal Kulikowski <michal.kulikowski@intel.com>
2016-07-04 15:59:08 +0200

    Added LRN.(Right now does not support multiple batches)
    Signed-off-by: Michal Kulikowski <michal.kulikowski@intel.com>

---
+++ src/gpu/lrn_gpu.cpp
@@ -0,0 +1,135 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "multidimensional_counter.h"
+#include "lrn_gpu.h"
+#include "kernel.h"
+
+const std::string kernelName = "lrn_GPU";
+const std::string kernelCode = R"__krnl(
+KERNEL (lrn_GPU)(__global neural_memory* input_mem, __global neural_memory* dst_mem, uint pSize, float k, float alpha, float beta, int helper_input_offset_feature)
+{
+    __global uint* input_size = get_raw(input_mem);
+    __global float* input = (__global float*)get_data(input_mem);
+    __global float* pDst = (__global float*)get_data(dst_mem);
+
+    const int idx = get_global_id(0);
+
+    const int ifm_num = input_size[1];
+    const int ifm_offset = idx % ifm_num;
+
+    const int x = idx / ifm_num;
+
+    float acc = 0;
+
+    for (int i = 0; i < pSize; i++)
+    {
+        int input_offset_f = i + ifm_offset + helper_input_offset_feature;
+        bool zero = false;
+        
+        zero = input_offset_f < 0 ? true : zero;
+        zero = input_offset_f >= ifm_num ? true : zero;
+
+        int input_idx = input_offset_f + x * ifm_num;
+        
+        float value = zero ? 0 : input[input_idx];
+        acc += value * value;
+        
+        //pDst[idx] = zero ? 0 : 1;
+    }
+    acc = acc * alpha + k;
+    acc = pow(acc, -beta);
+
+    pDst[idx] = acc * input[idx];
+}
+)__krnl";
+
+namespace neural {
+
+    lrn_gpu::lrn_gpu(normalization::response &arg)
+        : is_an_implementation(neural::type_id<lrn_gpu>())
+        , outer(arg) {};
+
+    lrn_gpu::~lrn_gpu() {};
+
+    void lrn_gpu::implementation(const void *ptr) {
+
+        auto this_lrn = static_cast<const normalization::response *>(ptr);
+
+        auto& input_offset = this_lrn->argument.input_offset;
+        auto& padding = this_lrn->argument.padding;
+        auto& size = this_lrn->argument.size;
+
+        auto& k = this_lrn->argument.k;
+        auto& alpha = this_lrn->argument.alpha;
+        auto& beta = this_lrn->argument.beta;
+
+        auto input_arg  = this_lrn->input_memory(0).argument;
+        auto output_arg = this_lrn->output_memory(0).argument;
+
+        if (input_arg.size.raw.size() != output_arg.size.raw.size())
+            throw std::runtime_error("lrn input/output number of dimension does not match [iput size=" + std::to_string(input_arg.size.raw.size())
+                                     + ", output size=" + std::to_string(output_arg.size.raw.size()));
+
+        vector<int32_t> help_input_offset({ input_offset });
+        help_input_offset.feature[0] -= static_cast<int32_t>(size / 2);
+
+        auto& input_mem = this_lrn->input_memory(0);
+        auto& output_mem = this_lrn->output_memory(0);
+        size_t dstSize = output_mem.count();
+
+        int lws = 16;
+        while (dstSize % lws)
+        {
+            lws--;
+        }
+
+        switch (padding) {
+        case padding::zero:
+        {
+            gpu::kernel<gpu::input_mem, gpu::output_mem, cl_uint, cl_float, cl_float, cl_float, cl_int> kernel(kernelName);
+            kernel({ dstSize, std::min(dstSize, (size_t)lws) }, input_mem, output_mem, size, k, alpha, beta, help_input_offset.feature[0]);
+            break;
+        }
+        default:
+            throw std::runtime_error("Unknown padding mode in lrn");
+        }        
+    }
+
+    namespace {
+        struct attach {
+            attach() {
+                gpu::kernel_templates::add(kernelName, kernelCode);
+                auto key = std::make_tuple(engine::gpu, memory::format::yxfb_f32, memory::format::yxfb_f32);
+                auto val_fw = lrn_gpu::create;
+                //auto val_bw = lrn_backward_cpu_reference::create;
+
+                lrn_fw_implementation_map::instance().insert({ key, val_fw }); //todo keys should be different
+                //lrn_bw_implementation_map.insert({ key, val_bw });
+            }
+            ~attach() {}
+        };
+
+#ifdef __GNUC__
+        __attribute__((visibility("default"))) //todo meybe dll_sym?
+#elif _MSC_VER
+#   pragma section(".nn_init$m", read, write)
+#endif
+        attach attach_impl;
+
+    }
+
+}
--- tests/test_cases/lrn_reference_test.cpp
+++ tests/test_cases/lrn_reference_test.cpp
@@ -85,3 +85,73 @@ TEST(local_response_normalization, lrn_test) {
     // test clean
 
 }
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+// GPU TESTS
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+TEST(local_response_normalization_gpu, lrn_test) {
+
+    using namespace neural;
+    using namespace tests;
+
+    // test initialization
+
+    // input-output parameters:
+
+    const uint32_t px = 2, py = 2, pb = 1, pf = 7, psize = 3;
+
+    std::initializer_list<float> input_oracle_init = {
+        -1.0f, -0.5f,  0.0f,  0.5f,  1.0f,  1.5f,  2.0f,    // b=0, x=0, y=0
+        -2.0f, -1.7f, -1.2f, -0.7f, -0.2f,  0.3f,  0.8f,    // b=0, x=1, y=0
+        0.1f,  0.4f,  0.9f,  1.4f,  1.9f,  2.4f,  2.9f,    // b=0, x=0, y=1
+        -10.0f, -8.0f, -7.5f, -7.0f, -6.5f, -6.0f, -5.5f };  // b=0, x=1, y=1
+
+    std::initializer_list<float> output_oracle_init = {
+        -0.54433f, -0.27217f,  0.00000f,  0.27217f,  0.32366f,  0.30814f,  0.45266f,    // b=0, x=0, y=0
+        -0.42484f, -0.31845f, -0.32025f, -0.30941f, -0.13928f,  0.19550f,  0.53034f,    // b=0, x=1, y=0
+        0.08889f,  0.23964f,  0.32244f,  0.31267f,  0.28876f,  0.26604f,  0.37728f,    // b=0, x=0, y=1
+        -0.21721f, -0.13945f, -0.15913f, -0.16455f, -0.17056f, -0.17725f, -0.23420f };  // b=0, x=1, y=1
+
+                                                                                        // lrn parameters:
+    const float pk = 1.0f, palpha = 1.0f, pbeta = 0.75f;
+
+    auto input = memory::allocate({ engine::gpu, memory::format::yxfb_f32,{ pb,{ px, py }, pf } });
+    auto output = memory::allocate({ engine::gpu, memory::format::yxfb_f32,{ pb,{ px, py }, pf } });
+    auto output_oracle = memory::allocate({ engine::gpu, memory::format::yxfb_f32,{ pb,{ px, py }, pf } });
+
+    set_values(input, input_oracle_init);
+    set_values(output_oracle, output_oracle_init);
+
+    auto lrn = normalization::response::create({ engine::gpu, output, input, psize, padding::zero, pk, palpha, pbeta });
+
+    // ------------------------------------------------------------------------------------------------
+    // test run
+    execute({ lrn }).wait();
+
+    // analysis of results
+    float* buff = nullptr;
+    float* buff_oracle = nullptr;
+
+    bool   result = true;
+
+    try {
+
+        buff = static_cast<float*>(output.as<const memory&>().pointer);
+        buff_oracle = static_cast<float*>(output_oracle.as<const memory&>().pointer);
+
+        for (size_t i = 0; i < px*py*pb*pf; ++i) {
+            EXPECT_NEAR(buff[i], buff_oracle[i], 1e-04F);
+        }
+    }
+    catch (const std::exception& E) {
+        std::cout << E.what() << std::endl;
+    }
+
+    EXPECT_EQ(true, result);
+    // ------------------------------------------------------------------------------------------------
+    // test clean
+
+}
