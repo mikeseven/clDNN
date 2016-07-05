3fa89a35ce2ff18d274ff870f5571bfceb5ce638
Michal Kulikowski <michal.kulikowski@intel.com>
2016-07-05 13:48:28 +0200

    Separated gpu tests from other tests.
    Signed-off-by: Michal Kulikowski <michal.kulikowski@intel.com>

---
+++ tests/test_cases/fully_connected_gpu_test.cpp
@@ -0,0 +1,118 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#include "api/neural.h"
+#include "multidimensional_counter.h"
+#include "tests/gtest/gtest.h"
+#include "test_utils/test_utils.h"
+#include "memory_utils.h"
+
+using namespace neural;
+using namespace tests;
+
+TEST(fully_connected_gpu, xb_f32_batch_2) {
+    //  Input  : 3x2
+    //  Output : 4x2
+    //  Weights: 4x3
+    //
+    //  Input:
+    //  -0.5     2    0.5
+    //   1       1.5  0
+    //
+    //  Weights:
+    //   1.5     1    0.5
+    //  -1       0    0.5
+    //   0.5    -0.5 -2
+    //  -0.5     1    1.5
+    //
+    //  Biases:
+    //   1.0, 2.0, 3.0, 4.0
+    //
+    //  Output:
+    //   2.5    2.75     0.75   7
+    //   4      1        2.75   5
+
+    const uint32_t output_x = 4, output_b = 2,  // size of whole output buffer
+        input_x = 3, input_b = 2,  // size of whole input buffer
+        weight_x = 4, weight_y = 3;  // size of whole weights buffer
+
+    auto input_prim   = memory::allocate({ engine::gpu, memory::format::xb_f32,{ input_b ,{ { input_x  } }, 1 } });
+    auto output_prim  = memory::allocate({ engine::gpu, memory::format::xb_f32,{ output_b,{ { output_x } }, 1 } });
+    auto weights_prim = memory::allocate({ engine::gpu, memory::format::xb_f32,{ weight_y,{ { weight_x } }, 1 } });
+    auto bias_prim =    memory::allocate({ engine::gpu, memory::format::x_f32, { 1,{ { output_x } }, 1 } });
+    auto full_con_prim = fully_connected::create({ engine::gpu, output_prim, input_prim, weights_prim, bias_prim });
+
+    set_values(input_prim, { -0.5f, 1.0f, 2.0f, 1.5f, 0.5f, 0.0f });
+    set_values(weights_prim, { 1.5f, 1.0f, 0.5f, -1.0f, 0.0f, 0.5f, 0.5f, -0.5f, -2.0f, -0.5f, 1.0f, 1.5f });
+    set_values(bias_prim, { 1.0f, 2.0f, 3.0f, 4.0f });
+
+    execute({ full_con_prim }).wait();
+
+    auto& output_memory = output_prim.as<const memory&>();
+    EXPECT_EQ(2.5f, get_value<float>(output_memory, 0));
+    EXPECT_EQ(4.0f, get_value<float>(output_memory, 1));
+    EXPECT_EQ(2.75f, get_value<float>(output_memory, 2));
+    EXPECT_EQ(1.0f, get_value<float>(output_memory, 3));
+    EXPECT_EQ(0.75f, get_value<float>(output_memory, 4));
+    EXPECT_EQ(2.75f, get_value<float>(output_memory, 5));
+    EXPECT_EQ(7.0f, get_value<float>(output_memory, 6));
+    EXPECT_EQ(5.0f, get_value<float>(output_memory, 7));
+}
+
+TEST(fully_connected_gpu, x_f32) {
+    //  Input  : 3x1
+    //  Output : 4x1
+    //  Weights: 4x3
+    //
+    //  Input:
+    //  -0.5     2    0.5
+    //
+    //  Weights:
+    //   1.5     1    0.5
+    //  -1       0    0.5
+    //   0.5    -0.5 -2
+    //  -0.5     1    1.5
+    //
+    //  Biases:
+    //   1.0, 2.0, 3.0, 4.0
+    //  Output:
+    //   2.5    2.75    0.75   7
+
+    const uint32_t output_x = 4,                 // size of whole output buffer
+        input_x = 3,                 // size of whole input buffer
+        weight_x = 4, weight_y = 3;  // size of whole weights buffer
+
+    auto input_prim   = memory::allocate({ engine::gpu, memory::format::x_f32, { 1       ,{ { input_x  } }, 1 } });
+    auto output_prim  = memory::allocate({ engine::gpu, memory::format::x_f32, { 1       ,{ { output_x } }, 1 } });
+    auto weights_prim = memory::allocate({ engine::gpu, memory::format::xb_f32,{ weight_y,{ { weight_x } }, 1 } });
+    auto bias_prim    = memory::allocate({ engine::gpu, memory::format::x_f32, { 1,{ { output_x } }, 1 } });
+
+    auto full_con_prim = fully_connected::create({ engine::gpu, output_prim, input_prim, weights_prim, bias_prim });
+
+    auto& output_memory = output_prim.as<const memory&>();
+
+    set_values(input_prim, { -0.5f, 2.0f, 0.5f });
+    set_values(weights_prim, { 1.5f, 1.0f, 0.5f, -1.0f, 0.0f, 0.5f, 0.5f, -0.5f, -2.0f, -0.5f, 1.0f, 1.5f });
+    set_values(bias_prim, { 1.0f, 2.0f, 3.0f, 4.0f });
+
+    execute({ full_con_prim }).wait();
+
+    EXPECT_EQ(2.5f, get_value<float>(output_memory, 0));
+    EXPECT_EQ(2.75f, get_value<float>(output_memory, 1));
+    EXPECT_EQ(0.75f, get_value<float>(output_memory, 2));
+    EXPECT_EQ(7.0f, get_value<float>(output_memory, 3));
+}
---
+++ tests/test_cases/relu_gpu_test.cpp
@@ -0,0 +1,90 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "api/neural.h"
+#include "tests/gtest/gtest.h"
+#include "test_utils/test_utils.h"
+#include "memory_utils.h"
+
+namespace{
+    auto calc_idx = [](std::vector<uint32_t> yxfb_pos, std::vector<uint32_t>& buf_size_bfyx) -> uint32_t{
+        return yxfb_pos[3]
+             + yxfb_pos[2] * buf_size_bfyx[0]
+             + yxfb_pos[1] * buf_size_bfyx[0] * buf_size_bfyx[1]
+             + yxfb_pos[0] * buf_size_bfyx[0] * buf_size_bfyx[1] * buf_size_bfyx[2];
+    };
+}
+
+using namespace neural;
+
+TEST(relu_f32_fw_gpu, basic) {
+    // FAIL now, because we don't support using the same buffer as input and output
+    EXPECT_EQ(false, true);
+    return;
+
+    const uint32_t y = 8, x = 8, f = 3, b = 2;
+
+    auto input = memory::allocate({ engine::gpu, memory::format::yxfb_f32,{ b,{ y, x }, f } });
+    auto output = memory::describe({ engine::gpu, memory::format::yxfb_f32,{ b,{ y, x }, f } });
+    fill<float>(input.as<const memory&>());
+
+    auto act = relu::create({ engine::gpu, output, input });
+    auto buf = static_cast<float*>(input.as<const memory&>().pointer);
+    // write output to input buffer
+    execute({ output(buf), act }).wait();
+
+    // multiply all positive intigers by -1
+    for (size_t i = 0; i < y*x*f*b; ++i)
+        buf[i] = (buf[i] > 0) ? -buf[i] : buf[i];
+
+    execute({ act }).wait();
+
+    bool result = false;
+    // every element should be 0.0f
+    for (size_t i = 0; i < y*x*f*b; ++i)
+        result = result || buf[i];
+
+    EXPECT_EQ(false, result);
+}
+
+TEST(relu_f32_fw_gpu, intrinsics_avx2) {
+    const uint32_t y = 8, x = 8, f = 3, b = 2;
+
+    // Optimized data
+    auto input = memory::allocate({ engine::gpu, memory::format::yxfb_f32,{ b,{ y, x }, f } });
+    auto output = memory::allocate({ engine::gpu, memory::format::yxfb_f32,{ b,{ y, x }, f } });
+    auto& input_memory = input.as<const memory&>();
+    auto& output_memory = output.as<const memory&>();
+
+    // Reference data
+    auto ref_output = memory::allocate({ engine::gpu, memory::format::yxfb_f32,{ b,{ y, x }, f } });
+    auto& ref_output_memory = ref_output.as<const memory&>();
+
+    // Initialize input data
+    fill<float>(input_memory);
+
+    // Relu primitives
+    auto opt_relu = relu::create({ engine::cpu, output, input });
+    auto ref_relu = relu::create({ engine::gpu, ref_output, input });
+
+    execute({ output, opt_relu }).wait();
+    execute({ ref_output, ref_relu }).wait();
+
+    for (size_t output_element = 0; output_element < output_memory.count(); ++output_element)
+        EXPECT_EQ(true, tests::are_equal(static_cast<float*>(ref_output_memory.pointer)[output_element], static_cast<float*>(output_memory.pointer)[output_element]));
+}
