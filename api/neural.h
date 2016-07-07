c7da58c7c662bb83fd9e31edee59e2bf3feee0ef
Michal Kulikowski <michal.kulikowski@intel.com>
2016-07-07 10:15:27 +0200

    Modified formatting of convolution_relu class.
    Signed-off-by: Michal Kulikowski <michal.kulikowski@intel.com>

--- api/neural.h
+++ api/neural.h
@@ -715,13 +715,13 @@ struct convolution_relu : is_a_primitive {
         neural::padding::type     padding;
         float                     negative_slope;
 
-        DLL_SYM arguments(neural::engine::type, neural::memory::format::type out_fmt, std::vector<primitive_at> in, neural::vector<int32_t> in_off, neural::vector<uint32_t> stride, neural::padding::type, float negative_slope);
-        DLL_SYM arguments(neural::engine::type, neural::memory::format::type out_fmt, std::vector<primitive_at> in, neural::vector<uint32_t> stride, neural::padding::type, float negative_slope);
-        DLL_SYM arguments(neural::engine::type, neural::memory::format::type out_fmt, std::vector<primitive_at> in, uint32_t                 stride, neural::padding::type, float negative_slope);
-        DLL_SYM arguments(neural::engine::type, neural::memory::format::type out_fmt, std::vector<primitive_at> in, neural::padding::type, float negative_slope);
-        DLL_SYM arguments(neural::engine::type, primitive                    out, std::vector<primitive_at> in, neural::vector<uint32_t> stride, neural::padding::type, float negative_slope);
-        DLL_SYM arguments(neural::engine::type, primitive                    out, std::vector<primitive_at> in, neural::padding::type, float negative_slope);
-        DLL_SYM arguments(neural::engine::type, primitive                    out, neural::vector<uint32_t> out_off, neural::vector<uint32_t> out_siz, std::vector<primitive_at> in, neural::vector<int32_t> in_off, neural::vector<uint32_t> stride, neural::padding::type);
+        DLL_SYM arguments(neural::engine::type, neural::memory::format::type out_fmt, std::vector<primitive_at>     in, neural::vector<int32_t>  in_off,                                                                neural::vector<uint32_t> stride, neural::padding::type, float negative_slope);
+        DLL_SYM arguments(neural::engine::type, neural::memory::format::type out_fmt, std::vector<primitive_at>     in,                                                                                                 neural::vector<uint32_t> stride, neural::padding::type, float negative_slope);
+        DLL_SYM arguments(neural::engine::type, neural::memory::format::type out_fmt, std::vector<primitive_at>     in,                                                                                                 uint32_t                 stride, neural::padding::type, float negative_slope);
+        DLL_SYM arguments(neural::engine::type, neural::memory::format::type out_fmt, std::vector<primitive_at>     in,                                                                                                                                  neural::padding::type, float negative_slope);
+        DLL_SYM arguments(neural::engine::type, primitive                        out, std::vector<primitive_at>     in,                                                                                                 neural::vector<uint32_t> stride, neural::padding::type, float negative_slope);
+        DLL_SYM arguments(neural::engine::type, primitive                        out, std::vector<primitive_at>     in,                                                                                                                                  neural::padding::type, float negative_slope);
+        DLL_SYM arguments(neural::engine::type, primitive                        out, neural::vector<uint32_t> out_off, neural::vector<uint32_t> out_siz, std::vector<primitive_at> in, neural::vector<int32_t> in_off, neural::vector<uint32_t> stride, neural::padding::type);
     };
     const arguments argument;
 
