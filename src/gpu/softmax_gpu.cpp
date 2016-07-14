edb51a2cbd8160172a28ebdee078b1b94678fbed
Michal Kulikowski <michal.kulikowski@intel.com>
2016-07-14 10:48:16 +0200

    Added softmax gpu implementation. Added softmax_gpu tests(Not all yet).
    Signed-off-by: Michal Kulikowski <michal.kulikowski@intel.com>

---
+++ src/gpu/softmax_gpu.cpp
@@ -0,0 +1,344 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "softmax_gpu.h"
+#include "multidimensional_counter.h"
+#include "kernel.h"
+
+const std::string kernelName = "softmax_gpu";
+const std::string kernelCode = R"__krnl(
+/*float find_max_value(const int idx, __global float* input)
+{
+    __local float partial_max[LWS];
+    float value = -FLT_MAX;
+    for(int i = 0; i < ITEMS_NUM; i++)
+    {
+        value = max(value, input[LWS * i + idx]);
+    }
+    value = max(value, idx < LEFTOVERS? LWS * ITEMS_NUM + idx : -FLT_MAX);
+    partial_max[idx] = value;
+
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    if(idx == 0)
+    {
+        for(int i = 1; i < LWS; i++)
+        {
+            partial_max[0] = max(partial_max[0], partial_max[i]);
+        };
+    }
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    return partial_max[0];
+}*/
+
+KERNEL (softmax_gpu)(__global neural_memory* input_mem, __global neural_memory* dst_mem)
+{
+    __global uint* input_size = get_raw(input_mem);
+    __global float* input = (__global float*)get_data(input_mem);
+    __global float* pDst = (__global float*)get_data(dst_mem);
+
+    const int idx = get_global_id(0);
+
+    const int batch_num = input_size[0];
+    const int batch_offset = idx % batch_num;
+
+    //float max_value = find_max_value(idx, input);
+    
+    // function code to be replaced with _find_max_value function
+    
+    
+    __local float partial_max[LWS];
+    float value = -FLT_MAX;
+    for(int i = 0; i < ITEMS_NUM; i++)
+    {
+        value = max(value, input[LWS * i + idx]);
+    }
+    value = max(value, idx < LEFTOVERS? LWS * ITEMS_NUM + idx : -FLT_MAX);
+    partial_max[idx] = value;
+
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    if(idx == 0)
+    {
+        for(int i = 1; i < LWS; i++)
+        {
+            partial_max[0] = max(partial_max[0], partial_max[i]);
+        };
+    }
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    float max_value = partial_max[0];
+
+
+
+
+    //
+    float tmp_vals[ITEMS_NUM + 1];
+    for(int i = 0; i < ITEMS_NUM; i++)
+    {
+        tmp_vals[i] = exp(input[LWS * i + idx] - max_value);
+    }
+    tmp_vals[ITEMS_NUM] = idx < LEFTOVERS ? exp(input[LWS * ITEMS_NUM + idx] - max_value) : 0;
+
+    // accumulate all values;
+    __local float partial_acc[LWS]; // all values accumulated;
+    partial_acc[idx] = 0;
+    for(int i = 0; i < ITEMS_NUM + 1; i++)
+    {
+        partial_acc[idx] += tmp_vals[i];
+    }
+
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE); // we must be sure that all threads calculated max of elements(we can remove it if simd32 and GWS <= 32
+#endif
+    if(idx == 0)
+    {
+        for(int i = 1; i < LWS; i++)
+        {
+            partial_acc[0] += partial_acc[i];
+        }
+    }
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    for(int i = 0; i < ITEMS_NUM; i++)
+    {
+        pDst[LWS * i + idx] = tmp_vals[i] / partial_acc[0];
+    }
+    if(idx < LEFTOVERS)
+        pDst[LWS * ITEMS_NUM + idx] = tmp_vals[ITEMS_NUM] / partial_acc[0];
+}
+)__krnl";
+
+const std::string kernelName2 = "softmax_gpu_batches";
+const std::string kernelCodeBatches = R"__krnl(
+/*float find_max_value(const int global_id, const int idx, const int batch_offset, const int batch_num, __global float* input)
+{
+    __local float partial_max[LWS];
+    float value = -FLT_MAX;
+    for(int i = 0; i < ITEMS_NUM; i++)
+    {
+        value = max(value, input[LWS * i + global_id]);
+    }
+    value = max(value, global_id < LEFTOVERS? LWS * ITEMS_NUM + global_id : -FLT_MAX);
+    partial_max[global_id] = value;
+
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    if(global_id < batch_num)
+    {
+        for(int i = 1; i < LWS / batch_num; i++)
+        {
+            partial_max[batch_offset] = max(partial_max[0], partial_max[i*batch_num + batch_offset]);
+        };
+    }
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    return partial_max[batch_offset];
+}*/
+
+KERNEL (softmax_gpu_batches)(__global neural_memory* input_mem, __global neural_memory* dst_mem)
+{
+    __global uint* input_size = get_raw(input_mem);
+    __global float* input = (__global float*)get_data(input_mem);
+    __global float* pDst = (__global float*)get_data(dst_mem);
+
+    const int batch_num = input_size[0];
+    const int global_id = get_global_id(0);
+    const int idx = global_id / batch_num;
+
+    const int batch_offset = global_id % batch_num;
+
+    //const float max_value = find_max_value(global_id, idx, batch_offset, batch_num, input);
+  
+
+    // function code to be replaced with _find_max_value function
+
+    __local float partial_max[LWS];
+    float value = -FLT_MAX;
+    for(int i = 0; i < ITEMS_NUM; i++)
+    {
+        value = max(value, input[LWS * i + global_id]);
+    }
+    value = max(value, global_id < LEFTOVERS? LWS * ITEMS_NUM + global_id : -FLT_MAX);
+    partial_max[global_id] = value;
+
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    if(global_id < batch_num)
+    {
+        for(int i = 1; i < LWS / batch_num; i++)
+        {
+            partial_max[batch_offset] = max(partial_max[0], partial_max[i*batch_num + batch_offset]);
+        };
+    }
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    const float max_value = partial_max[batch_offset];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //
+
+
+
+
+
+
+    float tmp_vals[ITEMS_NUM + 1];
+    for(int i = 0; i < ITEMS_NUM; i++)
+    {
+        tmp_vals[i] = exp(input[LWS * i + global_id] - max_value);
+    }
+    tmp_vals[ITEMS_NUM] = global_id < LEFTOVERS ? exp(input[LWS * ITEMS_NUM + global_id] - max_value) : 0;
+
+    // accumulate all values;
+    __local float partial_acc[LWS]; // all values accumulated;
+    partial_acc[global_id] = 0;
+    for(int i = 0; i < ITEMS_NUM + 1; i++)
+    {
+        partial_acc[global_id] += tmp_vals[i];
+    }
+
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE); // we must be sure that all threads calculated max of elements(we can remove it if simd32 and GWS <= 32
+#endif
+    if(global_id < batch_num)
+    {
+        for(int i = 1; i < LWS/batch_num; i++)
+        {
+            partial_acc[batch_offset] += partial_acc[i*batch_num + batch_offset];
+        }
+    }
+#if (GWS == LWS) && (LWS <= 32) 
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    for(int i = 0; i < ITEMS_NUM; i++)
+    {
+        pDst[LWS * i + global_id] = tmp_vals[i] / partial_acc[batch_offset];
+    }
+    if(global_id < LEFTOVERS)
+        pDst[LWS * ITEMS_NUM + global_id] = tmp_vals[ITEMS_NUM] / partial_acc[batch_offset];
+}
+)__krnl";
+namespace neural {
+namespace normalization {
+
+    softmax_gpu::softmax_gpu(softmax &arg)
+    : is_an_implementation(neural::type_id<softmax_gpu>())
+    , outer(arg) {}
+
+    softmax_gpu::~softmax_gpu() {}
+
+void softmax_gpu::implementation(const void *ptr) {
+    auto this_softmax = static_cast<const softmax *>(ptr);
+
+    auto& output_size   = this_softmax->argument.output_size;
+
+    assert( 1 == output_size.feature.size() );
+    assert( 1 == output_size.batch.size()   );
+
+    auto& input_mem = this_softmax->input_memory(0);
+    auto& output_mem = this_softmax->output_memory(0);
+    size_t dstSize = output_mem.count();
+
+    size_t batch_num = output_size.batch[0];
+    if (batch_num == 1)
+    {
+        const size_t preferred_lws = dstSize < 32 ? dstSize : 32;
+        const size_t preferred_gws = dstSize < 32 ? dstSize : dstSize - (dstSize % preferred_lws);
+        const size_t items_num = preferred_gws / preferred_lws;
+        const size_t leftovers = dstSize < 32 ? 0 : dstSize % preferred_lws;
+
+        gpu::jit_constants mem_consts{
+            gpu::make_jit_constant("ITEMS_NUM", std::to_string(items_num)),
+            gpu::make_jit_constant("LWS", std::to_string(preferred_lws)),
+            gpu::make_jit_constant("LEFTOVERS", std::to_string(leftovers))
+        };
+
+        gpu::kernel<gpu::input_mem, gpu::output_mem> kernel(kernelName, mem_consts);
+        kernel({ preferred_gws, preferred_lws }, input_mem, output_mem);
+    }
+    else
+    {
+        size_t _preferred_lws = batch_num;
+        size_t _items_num = dstSize / _preferred_lws;
+        while (_items_num > 32 || _preferred_lws < _items_num)
+        {
+            _preferred_lws <<= 1;
+            _items_num >>= 1;
+        }
+        //size_t _preferred_gws = dstSize < 32 ? dstSize : dstSize - (dstSize % _preferred_lws);
+        size_t _leftovers = dstSize % _preferred_lws;
+
+        gpu::jit_constants mem_consts{
+            gpu::make_jit_constant("ITEMS_NUM", std::to_string(_items_num)),
+            gpu::make_jit_constant("LWS", std::to_string(_preferred_lws)),
+            gpu::make_jit_constant("LEFTOVERS", std::to_string(_leftovers))
+        };
+
+        gpu::kernel<gpu::input_mem, gpu::output_mem> kernel(kernelName2, mem_consts);
+        kernel({ _preferred_lws, _preferred_lws }, input_mem, output_mem);
+    }
+}
+
+namespace {
+struct attach {
+    attach() {
+        gpu::kernel_templates::add(kernelName, kernelCode);
+        gpu::kernel_templates::add(kernelName2, kernelCodeBatches);
+
+        auto key_fw = std::make_tuple(engine::gpu, memory::format::xb_f32, memory::format::xb_f32);
+        auto val_fw = softmax_gpu::create;
+
+        softmax_fw_implementation_map::instance().insert( {key_fw, val_fw} );
+    }
+    ~attach() {}
+};
+}
+
+#ifdef __GNUC__
+    __attribute__((visibility("default"))) //todo meybe dll_sym?
+#elif _MSC_VER
+#   pragma section(".nn_init$m", read, write)
+#endif
+attach attach_impl;
+
+} // namespace normalization
+} // namespace neural
---
+++ tests/test_cases/softmax_gpu_test.cpp
@@ -0,0 +1,378 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "api/neural.h"
+#include "tests/gtest/gtest.h"
+#include "test_utils/test_utils.h"
+#include "memory_utils.h"
+#include <numeric>
+
+using namespace neural;
+using namespace std;
+using namespace tests;
+
+
+class softmax_gpu_xb_f32_test_fixture: public ::testing::Test {
+public:
+    static const uint32_t
+        output_x  = 10, output_b  = 2,  // size of whole output buffer
+        input_x   = 10, input_b   = 2,  // size of whole input buffer
+        in_size   = input_x*input_b,
+        out_size  = output_x*output_b;
+
+
+    float in_buffer[in_size];
+    float out_buffer[out_size];
+    float expected_buffer[out_size];
+
+    neural::primitive input  = memory::allocate({engine::gpu, memory::format::xb_f32, {input_b, {{input_x}}, 1}});
+    neural::primitive output = memory::allocate({engine::gpu, memory::format::xb_f32, {output_b, {{output_x}}, 1}});
+    neural::primitive act    = normalization::softmax::create({engine::gpu, output, input});
+
+    void compare_out_buffer_with_expected() {
+        for(size_t i = 0; i < out_size; ++i) {
+            // does output have expected values
+            EXPECT_TRUE(are_equal(out_buffer[i], expected_buffer[i]))
+                << "At ["<< i <<  "] Expected : " << expected_buffer[i] << " actual : " << out_buffer[i];
+        }
+    }
+
+    void compare_out_buffer_with_expected_batch_wise() {
+        for(size_t b = 0; b < output_b; ++b) {
+            float batch_wise_sum = 0;
+            for(size_t x = 0; x < output_x; ++x) {
+                auto idx = b+x*output_b;
+                batch_wise_sum += out_buffer[idx];
+                // does output have expected values
+                EXPECT_TRUE(are_equal(out_buffer[idx], expected_buffer[idx]))
+                    << "At ["<< idx <<  "] Expected : " << expected_buffer[idx] << " actual : " << out_buffer[idx];
+            }
+            // does it sum to 1 batch wise
+            EXPECT_TRUE(are_equal(batch_wise_sum, 1.0f))
+                << "Expected : " << 1.0f << " actual : " << batch_wise_sum;
+        }
+    }
+};
+
+TEST_F(softmax_gpu_xb_f32_test_fixture, input_same_values) {
+// in_buffer filled with same value == 1.0f
+    for(size_t i = 0; i < out_size; ++i) {
+              in_buffer[i] = 1.0f;
+        expected_buffer[i] = 0.1f;
+    }
+    std::vector<float> in_b(std::begin(in_buffer), std::end(in_buffer));
+
+    set_values(input, in_b);
+    execute({input, output, act}).wait();
+
+    auto& output_memory = output.as<const memory&>();
+    for (int i = 0; i < out_size; i++)
+    {
+        out_buffer[i] = get_value<float>(output_memory, i);
+    }
+    compare_out_buffer_with_expected();
+}
+
+TEST_F(softmax_gpu_xb_f32_test_fixture, input_same_values_batch_wise) {
+// in_buffer filled with same value == 1..2 each batch accordingly (softmax can only xb_f32 )
+    for(size_t i = 0; i < output_x; ++i) {
+        for(size_t j = 0; j < output_b; ++j)
+            in_buffer[j+i*output_b] = (j+i*output_b) % 2 +1.0f;
+    }
+
+    std::vector<float> in_b(std::begin(in_buffer), std::end(in_buffer));
+    set_values(input, in_b);
+    // fill buffer with the expected 0.1f value
+    for(size_t i = 0; i < out_size; ++i)
+        expected_buffer[i] = 0.1f;
+
+    execute({input, output, act}).wait();
+
+    auto& output_memory = output.as<const memory&>();
+    for (int i = 0; i < out_size; i++)
+    {
+        out_buffer[i] = get_value<float>(output_memory, i);
+    }
+    compare_out_buffer_with_expected_batch_wise();
+}
+
+TEST_F(softmax_gpu_xb_f32_test_fixture, values_batch_wise) {
+
+    float in_buf[in_size] = {
+       //b0  b1
+        2.0f, 2.0f, //x0
+        2.0f, 2.0f, //x1
+        2.0f, 2.0f, //x2
+        3.0f, 3.0f, //x3
+        5.0f, 5.0f, //x4
+        4.0f, 4.0f, //x5
+        3.0f, 3.0f, //x6
+        2.0f, 2.0f, //x7
+        2.0f, 2.0f, //x8
+        2.0f, 2.0f  //x9
+    };
+
+    float exp_buf[out_size] = {
+        0.02569957f,	 0.02569957f,
+        0.02569957f,	 0.02569957f,
+        0.02569957f,	 0.02569957f,
+        0.069858674f,    0.069858674f,
+        0.516189665f,    0.516189665f,
+        0.189895565f,    0.189895565f,
+        0.069858674f,    0.069858674f,
+        0.02569957f,	 0.02569957f,
+        0.02569957f,	 0.02569957f,
+        0.02569957f,	 0.02569957f
+
+    };
+
+    std::vector<float> in_b(std::begin(in_buf), std::end(in_buf));
+    set_values(input, in_b);
+    std::copy(exp_buf, exp_buf+in_size, expected_buffer);
+
+    // out_buffer filled with non-signaling NaN
+    for(size_t i = 0; i < out_size; ++i)
+        out_buffer[i] = NAN;
+
+    execute({input, output, act}).wait();
+    auto& output_memory = output.as<const memory&>();
+    for (int i = 0; i < out_size; i++)
+    {
+        out_buffer[i] = get_value<float>(output_memory, i);
+    }
+    compare_out_buffer_with_expected_batch_wise();
+}
+
+//TEST(softmax_gpu_xb_f32_test, basic_with_offsets) {
+//
+//    const uint32_t output_x  = 7, output_b  = 3,  // size of whole output buffer
+//                   input_x   = 6, input_b   = 2,  // size of whole input buffer
+//                   out_off_x = 0, out_off_b = 1,
+//                   out_siz_x = 5, out_siz_b = 2;  // size of area to do softmax after offset
+//
+//    const int32_t  in_off_x  = 1, in_off_b  = 0;
+//
+//    float in_buffer[input_x*input_b];
+//    float out_buffer[output_x*output_b];
+//    // input buffer should be initialized with valid data
+//
+//    auto input  = memory::allocate({engine::gpu, memory::format::xb_f32, {input_b, {{input_x}}, 1}});
+//    auto output = memory::allocate({engine::gpu, memory::format::xb_f32, {output_b, {{output_x}}, 1}});
+//
+//    auto act    = normalization::softmax::create({engine::gpu,
+//                                                  output,
+//                                                  {out_off_b, {{out_off_x}}, 0},
+//                                                  {out_siz_b, {{out_siz_x}}, 1},
+//                                                  input,
+//                                                  {in_off_b, {{in_off_x}}, 0}
+//                                                 });
+//    // in_buffer filled with same value == 1.0f
+//    for(size_t i = 0; i < input_x*input_b; ++i)
+//        in_buffer[i] = 1.0f;
+//
+//    std::vector<float> in_b(std::begin(in_buffer), std::end(in_buffer));
+//    set_values(input, in_b);
+//
+//    const float out_of_offset_value = NAN;
+//    // out_buffer filled with non-signaling NaN
+//    for(size_t i = 0; i < output_x*output_b; ++i)
+//        out_buffer[i] = out_of_offset_value;
+//
+//    std::vector<float> out_b(std::begin(out_buffer), std::end(out_buffer));
+//    set_values(output, out_b);
+//
+//    execute({input, output, act}).wait();
+//
+//    auto& output_memory = output.as<const memory&>();
+//    for (int i = 0; i < output_x*output_b; i++)
+//    {
+//        out_buffer[i] = get_value<float>(output_memory, i);
+//    }
+//
+//    auto expected_value = 0.2f;
+//    auto end_b = out_off_b+out_siz_b;
+//    auto end_x = out_off_x+out_siz_x;
+//
+//    for(size_t x = 0; x < output_x; ++x)
+//        for(size_t b = 0; b < output_b; ++b) {
+//            auto idx = b+x*output_b;
+//            float value = out_buffer[idx];
+//            float expected = (b >= out_off_b && b < end_b) && (x >= out_off_x && x < end_x) //is in range ?
+//                ? expected_value       // valid value that's in data range
+//                : out_of_offset_value; // invalid value (non-signaling NaN) for skipped buffer positions (bof offsets)
+//          EXPECT_TRUE(are_equal(value, expected))
+//              << "At ["<< idx <<  "] Expected : " << expected << " actual :" << value;
+//        }
+//};
+//
+//TEST(softmax_xb_f32_fw, intrinsics_avx2_batch1_sum_to_one) {
+//    const uint32_t x = 1000, b = 1;
+//
+//    auto input  = memory::allocate({engine::reference, memory::format::xb_f32, {b, {x}}});
+//    auto output = memory::allocate({engine::reference, memory::format::xb_f32, {b, {x}}});
+//    auto& input_memory  = input.as<const memory&>();
+//    auto output_memory_ptr = static_cast<float*>(output.as<const memory&>().pointer);
+//
+//    // Initialize input data
+//    fill<float>(input_memory);
+//
+//    auto softmax = normalization::softmax::create({engine::cpu, output, input});
+//
+//    execute({output, softmax}).wait();
+//
+//    auto sum = accumulate(output_memory_ptr, output_memory_ptr + x, 0.0f);
+//
+//    EXPECT_EQ(true, tests::are_equal(sum, 1.0f));
+//}
+//
+//TEST(softmax_xb_f32_fw, intrinsics_avx2_batch1_ref_compare) {
+//    const uint32_t x = 100, b = 1;
+//
+//    // Optimized data
+//    auto input  = memory::allocate({ engine::reference, memory::format::xb_f32, {b, {x}} });
+//    auto output = memory::allocate({ engine::reference, memory::format::xb_f32, {b, {x}} });
+//    auto& input_memory  = input.as<const memory&>();
+//    auto& output_memory = output.as<const memory&>();
+//
+//    // Reference data
+//    auto ref_output = memory::allocate({ engine::reference, memory::format::xb_f32,{b, {x}} });
+//    auto& ref_output_memory = ref_output.as<const memory&>();
+//
+//    // Initialize input data
+//    fill<float>(input_memory);
+//
+//    // Softmax primitives
+//    auto opt_softmax = normalization::softmax::create({ engine::cpu, output, input });
+//    auto ref_softmax = normalization::softmax::create({ engine::reference, ref_output, input });
+//
+//    execute({output, opt_softmax}).wait();
+//    execute({ref_output, ref_softmax}).wait();
+//
+//    for(uint32_t output_element = 0; output_element < output_memory.count(); ++output_element)
+//        EXPECT_EQ(true, tests::are_equal(get_value<float>(ref_output_memory, output_element), get_value<float>(output_memory, output_element)));
+//}
+//
+//TEST(softmax_xb_f32_fw, intrinsics_avx2_batch8_sum_to_one) {
+//    const uint32_t x = 1000, b = 8;
+//
+//    auto input  = memory::allocate({engine::reference, memory::format::xb_f32, {b, {x}}});
+//    auto output = memory::allocate({engine::reference, memory::format::xb_f32, {b, {x}}});
+//    auto& input_memory  = input.as<const memory&>();
+//    auto output_memory_ptr = static_cast<float*>(output.as<const memory&>().pointer);
+//
+//    // Initialize input data
+//    fill<float>(input_memory);
+//
+//    auto softmax = normalization::softmax::create({engine::cpu, output, input});
+//
+//    execute({output, softmax}).wait();
+//
+//    // Addition per batch
+//    bool result = true;
+//    for(uint32_t b_idx = 0; b_idx < b; ++b_idx) {
+//        float sum = 0;
+//        for(uint32_t x_idx = 0; x_idx < x; ++x_idx) {
+//            sum += output_memory_ptr[x_idx * b + b_idx];
+//        }
+//        result = tests::are_equal(sum, 1.0f);
+//        if(!result) break;
+//    }
+//
+//    EXPECT_EQ(true, result);
+//}
+//
+//TEST(softmax_xb_f32_fw, intrinsics_avx2_batch8_ref_compare) {
+//    const uint32_t x = 100, b = 8;
+//
+//    // Optimized data
+//    auto input  = memory::allocate({ engine::reference, memory::format::xb_f32, {b, {x}} });
+//    auto output = memory::allocate({ engine::reference, memory::format::xb_f32, {b, {x}} });
+//    auto& input_memory  = input.as<const memory&>();
+//    auto& output_memory = output.as<const memory&>();
+//
+//    // Reference data
+//    auto ref_output = memory::allocate({ engine::reference, memory::format::xb_f32,{b, {x}} });
+//    auto& ref_output_memory = ref_output.as<const memory&>();
+//
+//    // Initialize input data
+//    fill<float>(input_memory);
+//
+//    // Softmax primitives
+//    auto opt_softmax = normalization::softmax::create({ engine::cpu, output, input });
+//    auto ref_softmax = normalization::softmax::create({ engine::reference, ref_output, input });
+//
+//    execute({output, opt_softmax}).wait();
+//    execute({ref_output, ref_softmax}).wait();
+//
+//    for(uint32_t output_element = 0; output_element < output_memory.count(); ++output_element)
+//        EXPECT_EQ(true, tests::are_equal(get_value<float>(ref_output_memory, output_element), get_value<float>(output_memory, output_element)));
+//}
+//
+//TEST(softmax_xb_f32_fw, intrinsics_avx2_batch48_sum_to_one) {
+//    const uint32_t x = 1000, b = 48;
+//
+//    auto input  = memory::allocate({engine::reference, memory::format::xb_f32, {b, {x}}});
+//    auto output = memory::allocate({engine::reference, memory::format::xb_f32, {b, {x}}});
+//    auto& input_memory  = input.as<const memory&>();
+//    auto output_memory_ptr = static_cast<float*>(output.as<const memory&>().pointer);
+//
+//    // Initialize input data
+//    fill<float>(input_memory);
+//
+//    auto softmax = normalization::softmax::create({engine::cpu, output, input});
+//
+//    execute({output, softmax}).wait();
+//
+//    // Addition per batch
+//    bool result = true;
+//    for(uint32_t b_idx = 0; b_idx < b; ++b_idx) {
+//        float sum = 0;
+//        for(uint32_t x_idx = 0; x_idx < x; ++x_idx) {
+//            sum += output_memory_ptr[x_idx * b + b_idx];
+//        }
+//        result = tests::are_equal(sum, 1.0f);
+//        if(!result) break;
+//    }
+//
+//    EXPECT_EQ(true, result);
+//}
+//
+//TEST(softmax_xb_f32_fw, intrinsics_avx2_batch48_ref_compare) {
+//    const uint32_t x = 100, b = 48;
+//
+//    // Optimized data
+//    auto input  = memory::allocate({ engine::reference, memory::format::xb_f32, {b, {x}} });
+//    auto output = memory::allocate({ engine::reference, memory::format::xb_f32, {b, {x}} });
+//    auto& input_memory  = input.as<const memory&>();
+//    auto& output_memory = output.as<const memory&>();
+//
+//    // Reference data
+//    auto ref_output = memory::allocate({ engine::reference, memory::format::xb_f32,{b, {x}} });
+//    auto& ref_output_memory = ref_output.as<const memory&>();
+//
+//    // Initialize input data
+//    fill<float>(input_memory);
+//
+//    // Softmax primitives
+//    auto opt_softmax = normalization::softmax::create({ engine::cpu, output, input });
+//    auto ref_softmax = normalization::softmax::create({ engine::reference, ref_output, input });
+//
+//    execute({output, opt_softmax}).wait();
+//    execute({ref_output, ref_softmax}).wait();
+//
+//    for(uint32_t output_element = 0; output_element < output_memory.count(); ++output_element)
+//        EXPECT_EQ(true, tests::are_equal(get_value<float>(ref_output_memory, output_element), get_value<float>(output_memory, output_element)));
+//}
