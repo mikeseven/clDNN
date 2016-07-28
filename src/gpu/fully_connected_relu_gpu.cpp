270ed395a18d25e932aa2ab6082d280a756f791a
Michal Kulikowski <michal.kulikowski@intel.com>
2016-07-28 13:12:22 +0200

    Added fully_connected_common_gpu to reduce code duplication.
    Signed-off-by: Michal Kulikowski <michal.kulikowski@intel.com>

--- src/gpu/fully_connected_relu_gpu.cpp
+++ src/gpu/fully_connected_relu_gpu.cpp
@@ -16,30 +16,20 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include "api/neural.h"
-#include "multidimensional_counter.h"
+#include "fully_connected_common_gpu.h"
 #include "fully_connected_relu.h"
 #include "kernel.h"
 
 const std::string kernelName = "Fully_Connected_Relu_GPU";
-const std::string kernelCode = R"__krnl(
-KERNEL (Fully_Connected_Relu_GPU)(__global neural_memory* input_mem, __global neural_memory* dst_mem, float negative_slope)
+const std::string kernelCode_Begin = R"__krnl(
+KERNEL (Fully_Connected_Relu_GPU)(__global neural_memory* input_mem, __global neural_memory* dst_mem)
 {
-    __global uint* input_size = get_raw(input_mem);
-    __global float* input = (__global float*)get_data(input_mem);
-    __global float* pDst = (__global float*)get_data(dst_mem);
+)__krnl";
+const std::string kernelCode_Relu = R"__krnl(
+pDst[x] = max(pDst[x], 0.0f) + NEGATIVE_SLOPE * min(pDst[x], 0.0f);
+)__krnl";
 
-    const int x = get_global_id(0);
-
-    pDst[x] = 0;
-    uint outXIdx = x / input_size[0];
-    uint inputBatchIdx = x % input_size[0];
-    uint weightBatchIdx = outXIdx * WEIGHTS_BATCH_NUM;
-    for (uint i = 0; i < input_size[2]; i++)
-    {
-        pDst[x] += input[i * input_size[0] + inputBatchIdx] * WEIGHTS[weightBatchIdx + i];
-    }
-    pDst[x] += BIASES[outXIdx];
-    pDst[x] = max(pDst[x], 0.0f) + negative_slope * min(pDst[x], 0.0f);
+const std::string kernelCode_End = R"__krnl(
 }
 )__krnl";
 
@@ -79,11 +69,12 @@ namespace neural {
             float negative_slope = this_fc->argument.negative_slope;
             gpu::jit_constants mem_consts{  
                 gpu::make_jit_constant("WEIGHTS", weight_mem),
-                gpu::make_jit_constant("BIASES", bias_mem)
+                gpu::make_jit_constant("BIASES", bias_mem),
+                gpu::make_jit_constant("NEGATIVE_SLOPE", std::to_string(negative_slope))
             };
 
-            gpu::kernel<gpu::input_mem, gpu::output_mem, float> _kernel(kernelName, mem_consts);
-            _kernel({ output_bufSize, output_bufSize }, input_mem, output_mem, negative_slope);
+            gpu::kernel<gpu::input_mem, gpu::output_mem> _kernel(kernelName, mem_consts);
+            _kernel({ output_bufSize, output_bufSize }, input_mem, output_mem);
         }
 
         task_group work() override {
@@ -98,7 +89,7 @@ namespace neural {
 namespace {
     struct attach {
         attach() {
-            gpu::kernel_templates::add(kernelName, kernelCode);
+            gpu::kernel_templates::add(kernelName, kernelCode_Begin + fully_connected_code + kernelCode_Relu + kernelCode_End);
             auto val_fw = fully_connected_relu_gpu::create;
             fully_connected_relu_fw_implementation_map::instance().insert({ std::make_tuple(engine::gpu, memory::format::xb_f32, memory::format::xb_f32), val_fw });
             fully_connected_relu_fw_implementation_map::instance().insert({ std::make_tuple(engine::gpu, memory::format::x_f32,  memory::format::x_f32), val_fw });
