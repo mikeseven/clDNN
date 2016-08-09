51dbfa4e8d078b21a1aab9c80832c96602bd8e57
Michal Kulikowski <michal.kulikowski@intel.com>
2016-08-09 16:09:06 +0200

    Added gpu implementation of mean_subtract primitive.
    Signed-off-by: Michal Kulikowski <michal.kulikowski@intel.com>

---
+++ src/gpu/mean_subtract_gpu.h
@@ -0,0 +1,32 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+
+#include "api/neural.h"
+
+namespace neural {
+    struct mean_subtract_gpu : is_an_implementation {
+        mean_subtract_gpu(mean_subtract &arg);
+        ~mean_subtract_gpu();
+        static void implementation(const void *ptr);
+
+        static is_an_implementation *create(mean_subtract &arg) { return new mean_subtract_gpu(arg); };
+        task_group work() { return{ { task{ implementation, &outer } }, schedule::single }; };
+
+        const mean_subtract &outer;
+    };
+}
