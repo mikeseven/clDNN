d4b1853b4ed612cdd1d063b7bded3261b914d8da
itay.zuker <itay.zuker@intel.com>
2016-11-14 11:17:26 +0200

    fixed an invalid int and size_t comparison

--- tests/test_cases/convolution_gpu_test.cpp
+++ tests/test_cases/convolution_gpu_test.cpp
@@ -29,9 +29,9 @@ using namespace neural;
 using namespace tests;
 
 using VF = std::vector<float>;		// vector
-using VVF = std::vector<VF>;		// matrix (vector of vector)
-using VVVF = std::vector<VVF>;		// features (vector of matrix)
-using VVVVF = std::vector<VVVF>;	// batch (vector of features)
+using VVF = std::vector<VF>;		// feature map
+using VVVF = std::vector<VVF>;		// 3d feature map
+using VVVVF = std::vector<VVVF>;	// batch of 3d feature maps
 
 VVF convolve(VVF &input, VVF &filter, size_t stride, float bias) {
 	VVF output;
@@ -52,32 +52,93 @@ VVF convolve(VVF &input, VVF &filter, size_t stride, float bias) {
 	return output;
 }
 
+/*VVF convolve(VVVF &input, VVVF &filter, size_t stride, float bias) {
+	VVF output;
+	size_t output_rows = 1 + (input.size() - filter.size()) / stride;
+	size_t output_columns = 1 + (input[0].size() - filter[0].size()) / stride;
+	output.assign(output_rows, VF(output_columns, bias));
+	for (size_t f = 0; f < )
+	for (size_t i = 0; i < output_rows; ++i) {
+		for (size_t j = 0; j < output_columns; ++j) {
+			float val = 0;
+			for (size_t k = 0; k < filter.size(); ++k) {
+				for (size_t l = 0; l < filter[0].size(); ++l) {
+					val += input[k + stride * i][l + stride * j] * filter[k][l];
+				}
+			}
+			output[i][j] += val;
+		}
+	}
+	return output;
+}*/
+
 VF flatten_mat(VVF &mat) {
 	VF vec(mat.size() * mat[0].size());
-	for (int i = 0; i < mat.size(); ++i) {
-		for (int j = 0; j < mat[0].size(); ++j) {
+	for (size_t i = 0; i < mat.size(); ++i) {
+		for (size_t j = 0; j < mat[0].size(); ++j) {
 			vec[i * mat[0].size() + j] = mat[i][j];
 		}
 	}
 	return vec;
 }
 
-template<typename T>
-std::vector<T> generate_random_vec(size_t n, T min, T max) {
-	static std::default_random_engine generator((unsigned int)std::time(nullptr));
-	std::uniform_real_distribution<T> distribution(min, max);
-	std::vector<T> vec(n);
-	for (int i = 0; i < n; ++i)
-		vec[i] = distribution(generator);
+VF flatten_4d(neural::memory::format::type format, VVVVF &data) {
+	size_t a = data.size();
+	size_t b = data[0].size();
+	size_t c = data[0][0].size();
+	size_t d = data[0][0][0].size();
+	VF vec(a * b * c * d, 0.0f);
+	size_t idx = 0;
+	if (format == memory::format::yxfb_f32) {
+		for (size_t yi = 0; yi < c; ++yi)
+			for (size_t xi = 0; xi < d; ++xi)
+				for (size_t fi = 0; fi < b; ++fi)
+					for (size_t bi = 0; bi < a; ++bi)
+						vec[idx++] = data[bi][fi][yi][xi];
+	}
+	else if (format == memory::format::oiyx_f32) {
+		for (size_t oi = 0; oi < a; ++oi)
+			for (size_t ii = 0; ii < b; ++ii)
+				for (size_t yi = 0; yi < c; ++yi)
+					for (size_t xi = 0; xi < d; ++xi)
+						vec[idx++] = data[oi][ii][yi][xi];
+	}
 	return vec;
 }
 
 template<typename T>
-std::vector<std::vector<T>> generate_random_mat(size_t n, size_t m, T min, T max) {
-	std::vector<std::vector<T>> mat(n);
-	for (int i = 0; i < n; ++i)
-		mat[i] = generate_random_vec(m, min, max);
-	return mat;
+std::vector<T> generate_random_1d(size_t a, T min, T max) {
+	static std::default_random_engine generator((unsigned int)std::time(nullptr));
+	std::uniform_real_distribution<T> distribution(min, max);
+	std::vector<T> v(a);
+	for (size_t i = 0; i < a; ++i)
+		v[i] = distribution(generator);
+	return v;
+}
+
+template<typename T>
+std::vector<std::vector<T>> generate_random_2d(size_t a, size_t b, T min, T max) {
+	std::vector<std::vector<T>> v(a);
+	for (size_t i = 0; i < a; ++i)
+		v[i] = generate_random_1d(b, min, max);
+	return v;
+}
+
+template<typename T>
+std::vector<std::vector<std::vector<T>>> generate_random_3d(size_t a, size_t b, size_t c, T min, T max) {
+	std::vector<std::vector<std::vector<T>>> v(a);
+	for (size_t i = 0; i < a; ++i)
+		v[i] = generate_random_2d(b, c, min, max);
+	return v;
+}
+
+// parameters order is assumed to be bfyx or oiyx
+template<typename T>
+std::vector<std::vector<std::vector<std::vector<T>>>> generate_random_4d(size_t a, size_t b, size_t c, size_t d, T min, T max) {
+	std::vector<std::vector<std::vector<std::vector<T>>>> v(a);
+	for (size_t i = 0; i < a; ++i)
+		v[i] = generate_random_3d(b, c, d, min, max);
+	return v;
 }
 
 // rounds floating point number, fraction precision should be in the range [0,23]
@@ -115,9 +176,9 @@ TEST(convolution_f32_fw_gpu, basic_wsiz2x2_wstr2x2_in4x4x1x1_nopad_random) {
 	//  rnd  rnd
 	//  rnd  rnd
 
-	VVF rnd_input = generate_random_mat<float>(4, 4, -10.0f, 10.0f);
-	VVF rnd_filter = generate_random_mat<float>(2, 2, -10.0f, 10.0f);
-	VF rnd_bias = generate_random_vec<float>(1, -10.0f, 10.0f);
+	VVF rnd_input = generate_random_2d<float>(4, 4, -10.0f, 10.0f);
+	VVF rnd_filter = generate_random_2d<float>(2, 2, -10.0f, 10.0f);
+	VF rnd_bias = generate_random_1d<float>(1, -10.0f, 10.0f);
 	VVF rnd_output = convolve(rnd_input, rnd_filter, 2, rnd_bias[0]);
 
 	VF rnd_input_vec = flatten_mat(rnd_input);
@@ -140,7 +201,7 @@ TEST(convolution_f32_fw_gpu, basic_wsiz2x2_wstr2x2_in4x4x1x1_nopad_random) {
 	execute({ conv }).wait();
 
 	auto output_ptr = output.as<const memory&>().pointer<float>();
-	for (int i = 0; i < rnd_output_vec.size(); ++i) {
+	for (size_t i = 0; i < rnd_output_vec.size(); ++i) {
 		float x = float_round(rnd_output_vec[i]), y = float_round(output_ptr[i]);
 		EXPECT_FLOAT_EQ(x, y);
 	}
@@ -189,6 +250,58 @@ TEST(convolution_f32_fw_gpu, basic_wsiz2x2_wstr2x2_in4x4x1x1_nopad) {
     EXPECT_FLOAT_EQ(9.0f, output_ptr[3]);
 }
 
+TEST(convolution_f32_fw_gpu, basic_wsiz2x2_wstr2x2_in2x2x1x2_nopad_random) {
+	//  Filter : 2x2
+	//  Stride : 2x2
+	//  Input  : 2x2x1x2
+	//  Output : 1x1x1x2
+	//
+	//  Input:
+	//  rnd  rnd    rnd  rnd
+	//  rnd  rnd    rnd  rnd
+	//
+	//  Filter:
+	//  -1.2  1.5
+	//   0.5 -0.5
+	//
+	//  Bias:
+	//  -1
+	//
+	//  Output:
+	//  3.65 -5.36
+	auto input = memory::allocate({ memory::format::yxfb_f32,{ 2,{ 2, 2 }, 1 } });
+	auto output = memory::allocate({ memory::format::yxfb_f32,{ 2,{ 1, 1 }, 1 } });
+	auto weights = memory::allocate({ memory::format::oiyx_f32,{ 1,{ 2, 2 },{ 1, 1 } } });
+	auto biases = memory::allocate({ memory::format::x_f32,{ 1,{ { 1 } } , 1 } });
+
+
+	size_t input_y = 2, input_x = 2, input_f = 1, input_b = 2;
+
+	VVVVF rnd_data = generate_random_4d<float>(input_b, input_f, input_y, input_x, -10.0f, 10.0f);
+	VVVVF rnd_filter = generate_random_4d<float>(1, 1, 2, 2, -10.0f, 10.0f);
+	VF rnd_bias = generate_random_1d<float>(1, -10.0f, 10.0f);
+	VVF rnd_output1 = convolve(rnd_data[0][0], rnd_filter[0][0], 2, rnd_bias[0]);
+	VVF rnd_output2 = convolve(rnd_data[1][0], rnd_filter[0][0], 2, rnd_bias[0]);
+
+	VF rnd_data_vec = flatten_4d(memory::format::yxfb_f32, rnd_data);
+	VF rnd_filter_vec = flatten_4d(memory::format::oiyx_f32, rnd_filter);
+
+	set_values(input, rnd_data_vec);
+	set_values(weights, rnd_filter_vec);
+	set_values(biases, rnd_bias);
+
+	auto conv = convolution::create({ output,{ input, weights, biases },{ 1,{ 2, 2 }, 1 }, padding::zero });
+
+	execute({ conv }).wait();
+
+	auto output_ptr = output.as<const memory&>().pointer<float>();
+	float x, y;
+	x = float_round(rnd_output1[0][0]), y = float_round(output_ptr[0]);
+	EXPECT_FLOAT_EQ(x, y);
+	x = float_round(rnd_output2[0][0]), y = float_round(output_ptr[1]);
+	EXPECT_FLOAT_EQ(x, y);
+}
+
 TEST(convolution_f32_fw_gpu, basic_wsiz2x2_wstr2x2_in2x2x1x2_nopad) {
     //  Filter : 2x2
     //  Stride : 2x2
@@ -196,8 +309,8 @@ TEST(convolution_f32_fw_gpu, basic_wsiz2x2_wstr2x2_in2x2x1x2_nopad) {
     //  Output : 1x1x1x2
     //
     //  Input:
-    //  0.5  1.5    2.3 -0.4
-    //  2.0  4.0    1.0  3.0
+    //  0.5   1.5    2.3 -0.4
+    //  2.0  -4.0    1.0  3.0
     //
     //  Filter:
     //  -1.2  1.5
