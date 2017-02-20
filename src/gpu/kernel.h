05b30ddc0fc573b89b777247107f4e331d1b0a8e
Etay Meiri <etay.meiri@intel.com>
2017-02-20 16:57:51 +0200

    Fixed a memory leak in kernel::run() - the returned event is not implicitly destroyed as intended.

--- src/gpu/kernel.h
+++ src/gpu/kernel.h
@@ -371,7 +371,9 @@ public:
             setArgs<0>(clkernel, std::forward<Args>(args)...);
             context()->queue().enqueueNDRangeKernel(clkernel, cl::NullRange, options.global_range(), options.local_range(), &events, &end_event);
         }
-        return new cldnn::event_impl(end_event);
+
+        cldnn::refcounted_obj_ptr<cldnn::event_impl> e(new cldnn::event_impl(end_event), false);
+        return e;
     }
 };
 
