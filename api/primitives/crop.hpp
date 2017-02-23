273c17250c428c4dc420f9c70f629898edab353e
Lukasz Dudziak <lukasz.dudziak@intel.com>
2017-02-23 13:23:13 +0100

    Add missing base class 'update_dto' calls.

--- api/primitives/crop.hpp
+++ api/primitives/crop.hpp
@@ -55,6 +55,7 @@ protected:
 
     void update_dto(dto& dto) const override
     {
+        primitive_base::update_dto(dto);
         dto.reference_input = reference_input.c_str();
         dto.offsets = offsets;
     }
