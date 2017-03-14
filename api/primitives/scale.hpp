be11296a7bf23473673598366fb2a3b19e8e2aa3
abialas <artur.bialas@intel.com>
2017-03-14 13:15:43 +0100

    scale documentation fixes

--- api/primitives/scale.hpp
+++ api/primitives/scale.hpp
@@ -29,15 +29,16 @@ namespace cldnn
 /// @{
 
 /// @brief Performs elementwise product of input and scale_input.
-/// Input size : 2x3x4x5(BFYX)
-///     Possible scale inputs sizes :
-///     2x3x4x5 - works the same as(axis == 0 == -4) in caffe
-///     1x3x4x5 - works the same as(axis == 1 == -3) in caffe
-///     1x1x4x5 - works the same as(axis == 2 == -2) in caffe
-///     1x1x1x5 - works the same as(axis == 3 == -1) in caffe
-///     1x1x1x1 - works the same as empty shape(scalar) in caffe
-/// When scale_input is the same as input, the behavior is the same as eltwise with product operation.
-/// Optionally it can also add provided biases by setting bias_term.
+/// @details Scale input dimension should be equal to input dimension or be 1 if it is not there.<br>
+/// Input size : 2x3x4x5(BFYX)<br>
+///     Possible scale inputs sizes :<br>
+///     2x3x4x5 - works the same as(axis == 0 == -4) in caffe<br>
+///     1x3x4x5 - works the same as(axis == 1 == -3) in caffe<br>
+///     1x1x4x5 - works the same as(axis == 2 == -2) in caffe<br>
+///     1x1x1x5 - works the same as(axis == 3 == -1) in caffe<br>
+///     1x1x1x1 - works the same as empty shape(scalar) in caffe<br>
+/// When scale_input is the same as input, the behavior is the same as @ref eltwise with product operation.<br>
+/// Optionally it can also add provided biases by setting bias_term.<br>
 struct scale : public primitive_base<scale, CLDNN_PRIMITIVE_DESC(scale)>
 {
     CLDNN_DECLATE_PRIMITIVE(scale)
