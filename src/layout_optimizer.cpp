1b9d4c2bfa35314b31b1aa633d5050a7ff5811ac
Walkowiak, Marcin <marcin.walkowiak@intel.com>
2017-06-28 15:01:35 +0200

    [clDNN][conv yxfb] Disabled YXFB optimization when output size is specified.

--- src/layout_optimizer.cpp
+++ src/layout_optimizer.cpp
@@ -31,7 +31,7 @@ layout_optimizer::layout_optimizer(refcounted_obj_ptr<engine_impl> eng, bool ena
 {
 }
 
-layout layout_optimizer::get_expected_layout(layout const& current_layout, data_type type, std::shared_ptr<const convolution>, boost::optional<layout> const& output_layout)
+layout layout_optimizer::get_expected_layout(layout const& current_layout, data_type type, std::shared_ptr<const convolution> prim, boost::optional<layout> const& output_layout)
 {
     auto expected_tensor = current_layout.size;
     auto expected_data_type = current_layout.data_type;
@@ -54,7 +54,7 @@ layout layout_optimizer::get_expected_layout(layout const& current_layout, data_
         break;
 
     case data_type::weights: //convolution weights
-        if (batch < 32 || expected_data_type != data_types::f16 || !_optimization_attributes.splitted_convolution)
+        if (batch < 32 || expected_data_type != data_types::f16 || !_optimization_attributes.splitted_convolution || prim->with_output_size)
         {
             expected_tensor = current_layout.size;
             expected_format = cldnn::format::os_iyx_osv16;
@@ -71,7 +71,7 @@ layout layout_optimizer::get_expected_layout(layout const& current_layout, data_
         if (current_layout.format.dimension() != 4)
             throw std::runtime_error("Convolution input not 4-dimensional?");
 
-        if (expected_data_type != data_types::f16 || batch < 32 || !_optimization_attributes.splitted_convolution)
+        if (expected_data_type != data_types::f16 || batch < 32 || !_optimization_attributes.splitted_convolution || prim->with_output_size)
         {
             expected_tensor = current_layout.size;
             expected_format = cldnn::format::bfyx;
