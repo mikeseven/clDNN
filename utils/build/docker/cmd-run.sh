3f2b7e58f7005b55ecb5b66cb87b1f1615276636
Walkowiak, Marcin <marcin.walkowiak@intel.com>
2017-08-29 17:07:49 +0200

    [clDNN][build] Updating docker run script after adding proxy configuration to created container (during run).

--- utils/build/docker/cmd-run.sh
+++ utils/build/docker/cmd-run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -p 9090:9090 -d --restart=unless-stopped centos7.0-dt4-j8-tca
+docker run -p 9090:9090 -d --restart=unless-stopped \
+    -e "http_proxy=http://proxy-chain.intel.com:911" \
+    -e "https_proxy=http://proxy-chain.intel.com:912" \
+    -e "ftp_proxy=http://proxy-chain.intel.com:911" \
+    -e "no_proxy=localhost,127.0.0.0/8,::1" \
+    centos7.0-dt4-j8-tca
