cf96e3e49dbdcc7e3754f68c6e34d8e950744583
Jacob Subag <jacob.subag@intel.com>
2017-08-30 14:48:38 +0300

    Updated generic sample with power measurements. Built with SHA-1: ca9174cbf34fb4232bc90719bc119fdbe0c6e06d

--- inference_engine/copy_ie_binaries.bat
+++ inference_engine/copy_ie_binaries.bat
@@ -1,1 +1,1 @@
-XCOPY "\\samba-users.igk.intel.com\samba\Users\leszczyn\clDNN_Validation\cldnn-val-31" "C:\Tests\IE\bin\IE" /Y  
+XCOPY "\\samba-users.igk.intel.com\samba\Users\leszczyn\clDNN_Validation\cldnn-val-34" "C:\Tests\IE\bin\IE" /Y  
