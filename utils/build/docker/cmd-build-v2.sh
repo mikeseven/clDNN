d0cb8f803243142364691ddb9feba0d45cb71069
Walkowiak, Marcin <marcin.walkowiak@intel.com>
2017-10-24 13:30:18 +0200

    [clDNN][build] Docker v2 for Linux (fix for optimized slicing of images in docker).

--- utils/build/docker/cmd-build-v2.sh
+++ utils/build/docker/cmd-build-v2.sh
@@ -4,4 +4,11 @@ docker build --build-arg TC_AGENT_USER=sys_gpudnna1 \
              --build-arg TC_AGENT_NAME=gklvbm-gpu-dnn-lnx-01 \
              --build-arg TC_SERVER_URL=https://teamcity01-igk.devtools.intel.com \
              --build-arg TC_AGENT_PROPS="my.req.linux.distro=Centos\nmy.req.linux.version=7.0.1406\nmy.req.linux.docker=True\nmy.req.can_run_command.git=True\nmy.req.can_run_command.python=True\nmy.req.can_run_command.python3=True" \
-             $1 -f $1/Dockerfile.centos7.0-devtoolset4-6-7-clang-tc-agent --tag centos7.0-dt467-j8-tca
+             $1 -f $1/Dockerfile.centos7.0-devtoolset4-6-7-clang-tc-agent --tag centos7.0-dt467-j8-tca:A-1.0
+
+docker build --build-arg TC_AGENT_USER=sys_gpudnna1 \
+             --build-arg TC_AGENT_NAME=gklvbm-gpu-dnn-lnx-02 \
+             --build-arg TC_SERVER_URL=https://teamcity01-igk.devtools.intel.com \
+             --build-arg TC_AGENT_PORT=9091 \
+             --build-arg TC_AGENT_PROPS="my.req.linux.distro=Centos\nmy.req.linux.version=7.0.1406\nmy.req.linux.docker=True\nmy.req.can_run_command.git=True\nmy.req.can_run_command.python=True\nmy.req.can_run_command.python3=True" \
+             $1 -f $1/Dockerfile.centos7.0-devtoolset4-6-7-clang-tc-agent --tag centos7.0-dt467-j8-tca:B-1.0
--- utils/build/docker/cmd-run-v2.sh
+++ utils/build/docker/cmd-run-v2.sh
@@ -5,4 +5,13 @@ docker run -p 9090:9090 -d --restart=unless-stopped \
     -e "https_proxy=http://proxy-chain.intel.com:912" \
     -e "ftp_proxy=http://proxy-chain.intel.com:911" \
     -e "no_proxy=localhost,127.0.0.0/8,::1" \
-    centos7.0-dt467-j8-tca
+    --name agent-lnx-01-centos7-dt467 \
+    centos7.0-dt467-j8-tca:A-1.0
+
+docker run -p 9091:9091 -d --restart=unless-stopped \
+    -e "http_proxy=http://proxy-chain.intel.com:911" \
+    -e "https_proxy=http://proxy-chain.intel.com:912" \
+    -e "ftp_proxy=http://proxy-chain.intel.com:911" \
+    -e "no_proxy=localhost,127.0.0.0/8,::1" \
+    --name agent-lnx-02-centos7-dt467 \
+    centos7.0-dt467-j8-tca:B-1.0
