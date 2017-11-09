fffab741154588ed65242cbacb68ff8519af4403
abialas <artur.bialas@intel.com>
2017-11-09 14:17:05 +0100

    disable adding new format

--- src/layout_optimizer.cpp
+++ src/layout_optimizer.cpp
@@ -102,19 +102,12 @@ layout layout_optimizer::get_expected_layout(layout const& current_layout, data_
         if (should_use_winograd_2x3_s1(prim, current_layout, output_or_weights_layout, _output_size_handling_enabled))
             return layout(expected_data_type, format::winograd_2x3_s1_data, expected_tensor);
 
-        if (current_layout.format == format::bfyx && output_or_weights_layout.size.spatial[0] == 1 && output_or_weights_layout.size.spatial[1] == 1 &&
-            prim->stride.spatial[0] == 1 && prim->stride.spatial[1] == 1 &&
-            prim->input_offset.spatial[0] == 0 && prim->input_offset.spatial[1] == 0)
-        {
-            if (!((current_layout.size.feature[0] % 8) == 0 && ((current_layout.size.spatial[0] * current_layout.size.spatial[1]) % 16) == 0 &&
-                current_layout.data_padding == padding{ { 0,0,0,0 }, 0 }))
-            {
-                expected_tensor = current_layout.size.transform(cldnn::format::bf8_xy16, 1);
-                expected_format = cldnn::format::bf8_xy16;
-            }
-        }
-        else if (layout_optimizer::convolution_bfyx_opt(current_layout, output_or_weights_layout, prim)
-            || (_output_size_handling_enabled && prim->with_output_size))
+        if (layout_optimizer::convolution_bfyx_opt(current_layout, output_or_weights_layout, prim)
+            || (_output_size_handling_enabled && prim->with_output_size) || 
+            (current_layout.format == format::bfyx && output_or_weights_layout.size.spatial[0] == 1 && output_or_weights_layout.size.spatial[1] == 1 &&
+                prim->stride.spatial[0] == 1 && prim->stride.spatial[1] == 1 &&
+                prim->input_offset.spatial[0] == 0 && prim->input_offset.spatial[1] == 0 && ((current_layout.size.feature[0] % 8) == 0 && ((current_layout.size.spatial[0] * current_layout.size.spatial[1]) % 16) == 0 &&
+                current_layout.data_padding == padding{ { 0,0,0,0 }, 0 })))
         {
             expected_tensor = current_layout.size;
             expected_format = cldnn::format::bfyx;
