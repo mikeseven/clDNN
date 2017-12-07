52b0aa75e9aa191c01627c39db86eee1fc928820
abialas <artur.bialas@intel.com>
2017-12-07 09:39:49 +0100

    Revert "added support to byxf in winograd kernel"
    This reverts commit 117fba2d0738b8607d3ca7e30b6665c8bdab1efe.

--- kernel_selector/core/actual_kernels/convolution/convolution_kernel_winograd_2x3_s1_fused.cpp
+++ kernel_selector/core/actual_kernels/convolution/convolution_kernel_winograd_2x3_s1_fused.cpp
@@ -29,9 +29,7 @@ namespace KernelSelector {
         k.EnableInputWeightsType(WeightsType::F16);
         k.EnableInputWeightsType(WeightsType::F32);
         k.EnableInputLayout(DataLayout::bfyx);
-        k.EnableInputLayout(DataLayout::byxf);
         k.EnableOutputLayout(DataLayout::bfyx);
-        k.EnableOutputLayout(DataLayout::byxf);
         k.EnableTensorOffset();
         k.EnableTensorPitches();
         k.EnableBatching();
