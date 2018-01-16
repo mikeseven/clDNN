af212931954a1e1ec0a4dc5d3c8fa5bd29172c14
tponieck <tomasz.poniecki@intel.com>
2018-01-16 12:17:37 +0100

    add regular also

--- src/program.cpp
+++ src/program.cpp
@@ -1141,7 +1141,7 @@ void program_impl::skipped_branch_memory_dependencies()
                     if ((*usr)->get_processing_num() > node->get_processing_num())
                     {
                         has_conflict = true;
-                        //node->add_memory_dependency(node2->id());
+                        node->add_memory_dependency(node2->id());
                         add_memory_dependency(node, node2);
                     }
                 }
@@ -1184,8 +1184,8 @@ void program_impl::oooq_memory_dependencies()
             {
                 for (auto nd2 = nd1 + 1; nd2 != sync_region.end(); nd2++)
                 {
-                    //(*nd1)->add_memory_dependency((*nd2)->id());
-                    //(*nd2)->add_memory_dependency((*nd1)->id());
+                    (*nd1)->add_memory_dependency((*nd2)->id());
+                    (*nd2)->add_memory_dependency((*nd1)->id());
                     add_memory_dependency(*nd1, *nd2);
                     add_memory_dependency(*nd2, *nd1);
                 }
