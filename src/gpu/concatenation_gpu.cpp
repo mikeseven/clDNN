975676a45b4047004161e7ef4e2c50fc06b95f9f
tponieck <tomasz.poniecki@intel.com>
2018-01-25 08:59:27 +0100

    merge fix

--- src/gpu/concatenation_gpu.cpp
+++ src/gpu/concatenation_gpu.cpp
@@ -103,8 +103,7 @@ namespace {
                 { std::make_tuple(engine_types::ocl, data_types::i8,  format::yxfb), concatenation_gpu::create },
                 { std::make_tuple(engine_types::ocl, data_types::f32, format::bfyx), concatenation_gpu::create },
                 { std::make_tuple(engine_types::ocl, data_types::f16, format::bfyx), concatenation_gpu::create },
-                { std::make_tuple(engine_types::ocl, data_types::i8,  format::bfyx), concatenation_gpu::create }
-                { std::make_tuple(engine_types::ocl, data_types::f16, format::bfyx), concatenation_gpu::create },
+                { std::make_tuple(engine_types::ocl, data_types::i8,  format::bfyx), concatenation_gpu::create },
                 { std::make_tuple(engine_types::ocl, data_types::f32, format::byxf), concatenation_gpu::create },
                 { std::make_tuple(engine_types::ocl, data_types::f16, format::byxf), concatenation_gpu::create }
             });
