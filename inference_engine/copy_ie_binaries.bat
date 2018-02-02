fe6c02df8d402a8b7c32df9004e4f6569343df7d
leszczyn <krzysztof.leszczynski@intel.com>
2018-02-02 08:45:09 +0100

    new IE version testing

--- inference_engine/copy_ie_binaries.bat
+++ inference_engine/copy_ie_binaries.bat
@@ -1,1 +1,1 @@
-XCOPY "\\samba-users.igk.intel.com\samba\Users\leszczyn\clDNN_Validation\cldnn-val-43\windows" "C:\Tests\IE\bin\IE" /Y  
+XCOPY "\\samba-users.igk.intel.com\samba\Users\leszczyn\clDNN_Validation\cldnn-val-44\windows" "C:\Tests\IE\bin\IE" /Y  
