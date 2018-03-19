db6ea0a6553844047fb3b15bf620626c0c36435f
tponieck <tomasz.poniecki@intel.com>
2018-03-19 13:51:10 +0100

    change IE to 49

--- inference_engine/copy_ie_binaries.sh
+++ inference_engine/copy_ie_binaries.sh
@@ -1,1 +1,1 @@
-sudo cp -Rf /mnt/samba/Users/leszczyn/clDNN_Validation/cldnn-val-47/linux/* /tmp/Tests/IE/bin/IE_LNX/
+sudo cp -Rf /mnt/samba/Users/leszczyn/clDNN_Validation/cldnn-val-49/linux/* /tmp/Tests/IE/bin/IE_LNX/
