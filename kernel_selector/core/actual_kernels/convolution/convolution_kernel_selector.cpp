fa4623f612800032bd4651707baf0b187d32a003
mkulikow <michal.kulikowski@intel.com>
2018-07-05 11:47:00 +0200

    Depthwise separable convolution for byxf_af32 input/output format.
    Signed-off-by: mkulikow <michal.kulikowski@intel.com>

--- kernel_selector/core/actual_kernels/convolution/convolution_kernel_selector.cpp
+++ kernel_selector/core/actual_kernels/convolution/convolution_kernel_selector.cpp
@@ -34,6 +34,8 @@
 #include "convolution_kernel_DPAS.h"
 #include "convolution_kernel_DPAS_blocks.h"
 #include "convolution_kernel_1x1_gemm_DPAS.h"
+#include "convolution_kernel_byxf_af32_depthwise.h"
+
 #include <iostream>
  
 namespace kernel_selector 
@@ -58,6 +60,7 @@ namespace kernel_selector
         Attach<ConvolutionKernel_DPAS>();
         Attach<ConvolutionKernel_DPAS_blocks>();
         Attach<ConvolutionKernel_1x1_gemm_dpas>();
+        Attach<ConvolutionKernel_byxf_af32_depthiwise>();
         //Attach<ConvolutionKernel_Tutorial>(); //In order to use this implementation for tutorial purposes please uncomment this line
     }
 
