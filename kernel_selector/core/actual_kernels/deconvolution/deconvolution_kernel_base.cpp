3e926838f43bde4a490b9e1769700688bb99fb2f
Marcin Penkowski <marcin.penkowski@intel.com>
2018-08-24 10:18:03 +0200

    Fix in deconvolution params

--- kernel_selector/core/actual_kernels/deconvolution/deconvolution_kernel_base.cpp
+++ kernel_selector/core/actual_kernels/deconvolution/deconvolution_kernel_base.cpp
@@ -114,12 +114,12 @@ namespace kernel_selector
             return{};
         }
 
-        auto cldnn_jit = GetJitConstants(orgParams);
-        auto entry_point = GetEntryPoint(kernelName, orgParams.layerID, options);
+        auto cldnn_jit = GetJitConstants(newParams);
+        auto entry_point = GetEntryPoint(kernelName, newParams.layerID, options);
         auto jit = CreateJit(kernelName, cldnn_jit, entry_point);
 
         auto& kernel = kd.kernels[0];
-        FillCLKernelData(kernel, runInfo, kernelName, jit, entry_point, ROUND_ROBIN, true, !orgParams.bias.empty());
+        FillCLKernelData(kernel, runInfo, kernelName, jit, entry_point, ROUND_ROBIN, true, !newParams.bias.empty());
         kernel.arguments.push_back({ ArgumentDescriptor::Types::SPLIT, 0 });
 
         kd.estimatedTime = runInfo.effiency;
