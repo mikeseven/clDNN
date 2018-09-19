a42d352ac2b21a3049b00b73cbfcd79f40b1b72d
Kuszner, Lukasz <lukasz.kuszner@intel.com>
2018-09-19 17:16:30 +0200

    remove unused methods
    Change-Id: Ic1eedc7366f26d0cb82f9a2daa1ae13e468b8c0e

--- src/include/program_node.h
+++ src/include/program_node.h
@@ -111,8 +111,6 @@ public:
     auto const& get_users() const { return reinterpret_cast<const std::list<const program_node*>&>(users); }
 
     bool has_next() const;
-    program_node* get_next() { auto itr = processing_itr; return (*++itr); }
-    const program_node* get_next() const { auto itr = processing_itr; return (*++itr); }
 
     std::unique_ptr<json_composite> desc_to_json() const;
 	std::unique_ptr<xml_composite> desc_to_xml() const;
