e4f4a54bf7e278b9ffd8110ac5b3d85c21d71a9b
Sciuk, Witold <witold.sciuk@intel.com>
2018-09-20 14:59:26 +0200

    remove error from test for negative valuses
    Change-Id: I380bf776b585e08c5ba1c99ddf2e6fd7ee464d20

--- tests/test_cases/activation_simple_gpu_test.cpp
+++ tests/test_cases/activation_simple_gpu_test.cpp
@@ -230,7 +230,8 @@ TEST(activation_f32_fw_gpu, basic_yxfb_all_functions)
                     EXPECT_FLOAT_EQ(std::exp((float)input_ptr[i]), output_ptr[i]);
                     break;
 				case activation_log2:
-					EXPECT_FLOAT_EQ(std::log2((float)input_ptr[i]), output_ptr[i]);
+					if (input_ptr[i] > 0) //logharitm exist only for positive real values
+						EXPECT_FLOAT_EQ(std::log2((float)input_ptr[i]), output_ptr[i]);
 					break;
                 default:
                     break;
