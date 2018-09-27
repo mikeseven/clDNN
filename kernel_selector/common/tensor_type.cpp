a068423240352d502b329d8656502709eb856a16
mkulikow <michal.kulikowski@intel.com>
2018-09-27 13:34:45 +0200

    New layout for weights for conv1x1 for GCA.
    Signed-off-by: mkulikow <michal.kulikowski@intel.com>

--- kernel_selector/common/tensor_type.cpp
+++ kernel_selector/common/tensor_type.cpp
@@ -68,6 +68,7 @@ namespace kernel_selector
             { 0, 1, 2, 3 },  // WeightsLayout::image_2d_weights_winograd_6x3_s1_fbxyb
             { 0, 1, 2, 3 },  // WeightsLayout::image_2d_weights_winograd_6x3_s1_xfbyb
             { 0, 1, 2, 3 },  // WeightsLayout::os_is_yx_isa8_osv8_isv4
+            { 1, 2, 0, 3 },  // WeightsLayout::is_o_yx_isv32
         } };
 
         NDims DataTensor::GetSimpleDims(const std::vector<size_t>& d, DataLayout l)
@@ -292,6 +293,10 @@ namespace kernel_selector
                 assert(newDims.size() == 4);
                 newDims[3] = RoundUp(newDims[3], 8);
                 newDims[2] = RoundUp(newDims[2], 32);
+                break;
+            case is_o_yx_isv32:
+                assert(newDims.size() == 4);
+                newDims[0] = RoundUp(newDims[0], 32);
                 break;
             default:
                 break;
