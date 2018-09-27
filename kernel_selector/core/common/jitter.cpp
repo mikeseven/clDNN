c6f0151e4a8b15bbab2f30dd5986cd64d0e4816b
mkulikow <michal.kulikowski@intel.com>
2018-09-27 12:10:17 +0200

    Moved declaration of UNIT_TYPE defines from *.cl to host.
    Signed-off-by: mkulikow <michal.kulikowski@intel.com>

--- kernel_selector/core/common/jitter.cpp
+++ kernel_selector/core/common/jitter.cpp
@@ -120,6 +120,82 @@ namespace kernel_selector {
         return definitions;
     }
 
+    JitConstants MakeUnitTypeJitConstants(Datatype dataType)
+    {
+        std::string unit_type;
+        std::string unit_max_val;
+        std::string unit_min_val;
+        std::string unit_val_one;
+        std::string unit_val_zero;
+        std::string to_unit_type;
+        std::string unit_max_func;
+        std::string unit_min_func;
+        switch (dataType)
+        {
+        case Datatype::INT8:
+            unit_type = "char";
+            unit_max_val = "CHAR_MAX";
+            unit_min_val = "-UNIT_VAL_MAX";
+            unit_val_one = "(char) 1";
+            unit_val_zero = "(char) 0";
+            to_unit_type = "convert_char(v)";
+            unit_max_func = "max";
+            unit_min_func = "min";
+            break;
+        case Datatype::INT32:
+            unit_type = "int";
+            unit_max_val = "INT_MAX";
+            unit_min_val = "-UNIT_VAL_MAX";
+            unit_val_one = "(int) 1";
+            unit_val_zero = "(int) 0";
+            to_unit_type = "convert_int(v)";
+            unit_max_func = "max";
+            unit_min_func = "min";
+            break;
+        case Datatype::INT64:
+            unit_type = "long";
+            unit_max_val = "LONG_MAX";
+            unit_min_val = "-UNIT_VAL_MAX";
+            unit_val_one = "(long) 1";
+            unit_val_zero = "(long) 0";
+            to_unit_type = "convert_long(v)";
+            unit_max_func = "max";
+            unit_min_func = "min";
+            break;
+        case Datatype::F16:
+            unit_type = "half";
+            unit_max_val = "HALF_MAX";
+            unit_min_val = "-UNIT_VAL_MAX";
+            unit_val_one = "1.0h";
+            unit_val_zero = "0.0h";
+            to_unit_type = "convert_half(v)";
+            unit_max_func = "fmax";
+            unit_min_func = "fmin";
+            break;
+        default:
+            unit_type = "float";
+            unit_max_val = "FLT_MAX";
+            unit_min_val = "-UNIT_VAL_MAX";
+            unit_val_one = "1.0f";
+            unit_val_zero = "0.0f";
+            to_unit_type = "(float)(v)";
+            unit_max_func = "fmax";
+            unit_min_func = "fmin";
+            break;
+        }
+
+        return JitConstants
+        {
+            MakeJitConstant("UNIT_TYPE",            unit_type),
+            MakeJitConstant("UNIT_VAL_MAX",         unit_max_val),
+            MakeJitConstant("UNIT_VAL_MIN",         unit_min_val),
+            MakeJitConstant("UNIT_VAL_ONE",         unit_val_one),
+            MakeJitConstant("UNIT_VAL_ZERO",        unit_val_zero),
+            MakeJitConstant("TO_UNIT_TYPE(v)",      to_unit_type),
+            MakeJitConstant("UNIT_MAX_FUNC",        unit_max_func),
+            MakeJitConstant("UNIT_MIN_FUNC",        unit_min_func),
+        };
+    }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // MakeBaseParamsJitConstants
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -137,16 +213,6 @@ namespace kernel_selector {
             bInt64Used |= i.GetDType() == Datatype::INT64;
         }
 
-        std::string unit_type = "float";
-        if (bInt8Used)
-            unit_type = "char";
-        else if (bFP16Used)
-            unit_type = "half";
-        else if (bInt32Used)
-            unit_type = "int";
-        else if (bInt64Used)
-            unit_type = "long";
-
         JitConstants jit{
             MakeJitConstant("OUTPUT",               params.output),
             MakeJitConstant("FP64_SUPPORTED",       params.engineInfo.bFP64Support),
@@ -155,12 +221,32 @@ namespace kernel_selector {
             MakeJitConstant("INT8_UNIT_USED",       bInt8Used),
             MakeJitConstant("INT32_UNIT_USED",      bInt32Used),
             MakeJitConstant("INT64_UNIT_USED",      bInt64Used),
-            MakeJitConstant("UNIT_TYPE",            unit_type),
             MakeJitConstant("NL_M",                 params.activationParams.m),
             MakeJitConstant("NL_N",                 params.activationParams.n),
             MakeJitConstant("ACTIVATION_FUNCTION_" + toString(params.activationFunc), ""),
             MakeJitConstant("GRADIENT",             params.gradient),
         };
+
+        if (bInt8Used)
+        {
+            jit.Merge(MakeUnitTypeJitConstants(Datatype::INT8));
+        }
+        else if (bFP16Used)
+        {
+            jit.Merge(MakeUnitTypeJitConstants(Datatype::F16));
+        }
+        else if (bInt32Used)
+        {
+            jit.Merge(MakeUnitTypeJitConstants(Datatype::INT32));
+        }
+        else if (bInt64Used)
+        {
+            jit.Merge(MakeUnitTypeJitConstants(Datatype::INT64));
+        }
+        else
+        {
+            jit.Merge(MakeUnitTypeJitConstants(Datatype::F32));
+        }
 
         for (size_t i = 0; i < params.inputs.size(); i++)
         {
