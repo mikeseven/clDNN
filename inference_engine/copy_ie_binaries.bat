28301671c5602dc45b295f5331cf57948fa3bbe4
Kuznetsov, Boris V <boris.v.kuznetsov@intel.com>
2018-10-12 14:07:58 +0200

    Add global (kernel size is equal to the spatial dimension of input tensor) pooling constructor.

--- inference_engine/copy_ie_binaries.bat
+++ inference_engine/copy_ie_binaries.bat
@@ -1,1 +1,1 @@
-XCOPY "\\samba-users.igk.intel.com\samba\Users\leszczyn\clDNN_Validation\cldnn-val-63\windows" "C:\Tests\IE\bin\IE" /Y  
+XCOPY "\\samba-users.igk.intel.com\samba\Users\leszczyn\clDNN_Validation\cldnn-val-64\windows" "C:\Tests\IE\bin\IE" /Y  
